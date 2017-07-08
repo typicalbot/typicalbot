c148d04bb04f7b5ddd2760f31976dd1db3f25b7e
HyperCoder2975 <bryan.pikaardjr@outlook.com>
2017-07-08 13:49:31 -0400

    Fix API Latency

--- Source/Commands/Utility/ping.js
+++ Source/Commands/Utility/ping.js
@@ -15,6 +15,6 @@ module.exports = class extends Command {
 
     async execute(message, response, permissionLevel) {
         const msg = await response.send("Pinging...");
-        msg.edit(`Command Execution Time : ${msg.createdTimestamp - message.createdTimestamp}ms | Discord API Latency : ${Math.floor(this.client.ping)}ms`);
+        msg.edit(`Command Execution Time : ${msg.createdTimestamp - message.createdTimestamp}ms | Discord API Latency : ${Math.floor(this.client.pings[0])}ms`);
     }
 };
