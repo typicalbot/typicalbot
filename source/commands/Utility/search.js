97362135aba0b3c2736b87071c422c03c3892806
HyperCoder2975 <bryan.pikaardjr@outlook.com>
2017-08-23 15:08:44 -0400

    Make search

---
+++ source/commands/Utility/search.js
@@ -0,0 +1,35 @@
+const Command = require("../../structures/Command");
+
+module.exports = class extends Command {
+    constructor(client, filePath) {
+        super(client, filePath, {
+            name: "search",
+            description: "Searches in the user list for a username or nickname.",
+            usage: "search <query>",
+            mode: "lite"
+        });
+    }
+
+    execute(message, response, permissionLevel) {
+        const args = /search\s+(\S+)(?:\s+(\d+))?/i.exec(message.content);
+        if (!args) return response.usage(this);
+
+        const query = args[1];
+        const page = args[2] || 1;
+
+        const list = message.guild.members.filter(m => {
+            if (m.user.username.toLowerCase().includes(query.toLowerCase())) return true;
+            if (m.nickname && m.nickname.toLowerCase().includes(query.toLowerCase())) return true;
+            return false;
+        });
+
+        if (!list.size) return response.reply(`There are no matches for the query **${query}**.`);
+
+        const content = this.client.functions.pagify(
+            list.map(m => m.nickname ? `${this.client.functions.lengthen(1, m.user.name, 30)} : ${m.nickname}` : m.user.username),
+            page
+        );
+
+        response.send(`**__Results:__**\n\n\`\`\`autohotkey\n${content}\n\`\`\``);
+    }
+};
