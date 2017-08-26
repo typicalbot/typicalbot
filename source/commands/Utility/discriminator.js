4f7b59888c7614b56e20ecbaeb431149bf2bc58a
HyperCoder <bryan.pikaardjr@outlook.com>
2017-08-25 22:24:09 -0400

    I lied...NOW last fix

--- source/commands/Utility/discriminator.js
+++ source/commands/Utility/discriminator.js
@@ -14,8 +14,8 @@ module.exports = class extends Command {
     execute(message, response, permissionLevel) {
         const args = /discrim(?:inator)?\s+#?(\d{4})(?:\s+(\d+))?/i.exec(message.content);
 
-        const discriminator = args[1] || message.author.discriminator;
-        const page = args[2] || 1;
+        const discriminator = args && args[1] ? args[1] : message.author.discriminator;
+        const page = args && args[2] ? args[2] : 1;
 
         const list = this.client.users.findAll("discriminator", discriminator);
         if (!list.length) return response.reply(`There are no users with the discriminator **${discriminator}** on record.`);
