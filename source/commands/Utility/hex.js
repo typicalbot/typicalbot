e7495634fc46adf095790e63820b9d343088e2f1
HyperCoder <bryan.pikaardjr@outlook.com>
2017-08-29 17:55:03 -0400

    Update Hex command

--- source/commands/Utility/hex.js
+++ source/commands/Utility/hex.js
@@ -1,31 +1,34 @@
 const Command = require("../../structures/Command");
-const Canvas = require("canvas-constructor");
+const { Canvas } = require("canvas-constructor");
+const { Attachment } = require("discord.js");
 
 module.exports = class extends Command {
     constructor(client, filePath) {
         super(client, filePath, {
             name: "hex",
             description: "Sends a preview of a hex color.",
-            usage: "search <query>",
+            usage: "hex <hex-color:0-9a-fA-F>",
             mode: "lite"
         });
     }
 
     execute(message, response, permissionLevel) {
-        const args = /hex\s+#?([\w\d]{6})/i.exec(message.content);
+        const args = /hex\s+#?([0-9a-fA-F]{6})/i.exec(message.content);
         if (!args) return response.usage(this);
 
         const hex = args[1];
 
-        message.channel.send({
-            attachment: new Canvas(300, 300)
-                .setColor(`#${hex}`)
-                .addRect(5, 5, 290, 290)
-                .setColor(`#000000`)
-                .setTextFont('28px Impact')
-                .setTextAlign('center')
-                .addText(`#${hex}`.toUpperCase(), 150, 150)
-                .toBuffer()
-        });
+        message.channel.send(
+            new Attachment(
+                new Canvas(200, 100)
+                    .setColor(`#${hex}`)
+                    .addRect(5, 5, 190, 90)
+                    .setColor(`#ffffff`)
+                    .setTextFont('20px Impact')
+                    .setTextAlign('right')
+                    .addText(`#${hex}`.toUpperCase(), 100, 90)
+                    .toBuffer()
+                )
+        );
     }
 };
