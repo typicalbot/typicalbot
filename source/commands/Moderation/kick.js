83a7e391c8ec92de6d470b39d0d7630449768380
HyperCoder <bryan.pikaardjr@outlook.com>
2017-09-24 09:53:07 -0400

    Fix kick's log

--- source/commands/Moderation/kick.js
+++ source/commands/Moderation/kick.js
@@ -24,15 +24,16 @@ module.exports = class extends Command {
             if (message.member.highestRole.position <= member.highestRole.position) return response.error(`You cannot kick a user with either the same or higher highest role.`);
             if (!member.kickable) return response.error(`In order to complete the request, I need the **KICK_MEMBERS** permission and my highest role needs to be the requested user's highest role.`);
 
-            member.kick().then(async actioned =>  {
+            member.kick().then(async actioned => {
                 if (message.guild.settings.logs.moderation) {
-                    const log = { "action": "kick", "user": member.user, "moderator": message.author };
+                    const log = { "action": "kick", "user": actioned.user, "moderator": message.author };
                     if (reason) Object.assign(log, { reason });
 
-                    const _case = await this.client.modlogsManager.createLog(message.guild, log);
+                    await this.client.modlogsManager.createLog(message.guild, log);
+
                     response.success(`Successfully kicked user \`${actioned.user.tag}\`.`);
-                } else return response.success(`Successfully kicked user **${member.user.tag}**.`);
-            }).catch(err => response.error(`An error occured while trying to kick the requested user.`));
+                } else return response.success(`Successfully kicked user **${actioned.user.tag}**.`);
+            }).catch(err => response.error(`An error occured while trying to kick the requested user.${message.author.id === "105408136285818880" ? `\n\n\`\`\`${err}\`\`\`` : ""}`));
         }).catch(err => response.error(`An error occured while trying to fetch the requested user.${message.author.id === "105408136285818880" ? `\n\n\`\`\`${err}\`\`\`` : ""}`));
     }
 };
