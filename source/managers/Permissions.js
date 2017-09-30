fc77b8e471cee9ab14767b4076244eec9d87cb63
HyperCoder <bryan.pikaardjr@outlook.com>
2017-09-30 15:10:14 -0400

    Remove useless variable

--- source/managers/Permissions.js
+++ source/managers/Permissions.js
@@ -1,4 +1,4 @@
-class PermissionLevel { constructor(level, globalPermissions, title, check) { this.level = level; this.global = globalPermissions; this.title = title; this.check = check ? check : () => { return true; }; } }
+class PermissionLevel { constructor(level, title, check) { this.level = level; this.title = title; this.check = check ? check : () => { return true; }; } }
 
 module.exports = class {
     constructor(client) {
@@ -6,27 +6,27 @@ module.exports = class {
 
         this.levels = {
 
-            "-1": new PermissionLevel(-1, false, "Server Blacklisted", (guild, member) => {
+            "-1": new PermissionLevel(-1, "Server Blacklisted", (guild, member) => {
                 const role = this.fetch(guild, "blacklist"); if (!role) return;
                 if (role instanceof Array) { let isRole = false; role.forEach(r => { if (member.roles.has(r.id)) isRole = true; }); return isRole; } else { return member.roles.has(role.id); }
             }),
-            "0" : new PermissionLevel(0, false, "Server Member"),
-            "1" : new PermissionLevel(1, false, "Server DJ", (guild, member) => {
+            "0" : new PermissionLevel(0, "Server Member"),
+            "1" : new PermissionLevel(1, "Server DJ", (guild, member) => {
                 const role = this.fetch(guild, "dj"); if (!role) return;
                 if (role instanceof Array) { let isRole = false; role.forEach(r => { if (member.roles.has(r.id)) isRole = true; }); return isRole; } else { return member.roles.has(role.id); }
             }),
-            "2" : new PermissionLevel(2, false, "Server Moderator", (guild, member) => {
+            "2" : new PermissionLevel(2, "Server Moderator", (guild, member) => {
                 const role = this.fetch(guild, "moderator"); if (!role) return;
                 if (role instanceof Array) { let isRole = false; role.forEach(r => { if (member.roles.has(r.id)) isRole = true; }); return isRole; } else { return member.roles.has(role.id); }
             }),
-            "3" : new PermissionLevel(3, false, "Server Administrator", (guild, member) => {
+            "3" : new PermissionLevel(3, "Server Administrator", (guild, member) => {
                 const role = this.fetch(guild, "administrator"); if (!role) return;
                 if (role instanceof Array) { let isRole = false; role.forEach(r => { if (member.roles.has(r.id)) isRole = true; }); return isRole; } else { return member.roles.has(role.id); }
             }),
-            "4" : new PermissionLevel(4, false, "Server Owner", (guild, member) => member.id === guild.ownerID),
-            "8" : new PermissionLevel(8, false, "TypicalBot Support", (guild, id) => this.client.config.support[id]),
-            "9" : new PermissionLevel(9, true, "TypicalBot Administrator", (guild, id) => this.client.config.administrators[id]),
-            "10" : new PermissionLevel(10, true, "TypicalBot Creator", (guild, id) => id === client.config.creator),
+            "4" : new PermissionLevel(4, "Server Owner", (guild, member) => member.id === guild.ownerID),
+            "8" : new PermissionLevel(8, "TypicalBot Support", (guild, id) => this.client.config.support[id]),
+            "9" : new PermissionLevel(9, "TypicalBot Administrator", (guild, id) => this.client.config.administrators[id]),
+            "10" : new PermissionLevel(10, "TypicalBot Creator", (guild, id) => id === client.config.creator),
 
         };
     }
