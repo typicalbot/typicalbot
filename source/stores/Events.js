28de3283feaadcc3165fffbb45854181257941bd
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-06 15:54:54 -0400

    New Stuff [2]

--- source/stores/Events.js
+++ source/stores/Events.js
@@ -1,10 +1,9 @@
 const Store = require("../structures/Store");
-const { join } = require("path");
 
 module.exports = class extends Store {
     constructor(client) {
-        super(client, join(__dirname, "..", "events"));
+        super(client, "../events");
 
-
+        this.loadAll();
     }
 };
