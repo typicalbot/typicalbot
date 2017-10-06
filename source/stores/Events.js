314b3d4457ebeccc55b66d529b90d548dc7bdcfe
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-06 16:27:55 -0400

    Fix loading

--- source/stores/Events.js
+++ source/stores/Events.js
@@ -1,11 +1,12 @@
 const Store = require("../structures/Store");
+const path = require("path");
 
 class EventStore extends Store {
     constructor(client) {
-        super(client, "events", "../events");
+        super(client, "events", path.join(__dirname, "..", "events"));
 
-        this.loadAll().then(events => {
-            events.forEach(e => {
+        this.loadAll().then(() => {
+            this.forEach(e => {
                 client.on(e.event, (...args) => e.execute(...args));
             });
         });
