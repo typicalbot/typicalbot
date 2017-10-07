f17607546013a905f4317e2cae644dab0ceb8148
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-07 08:57:13 -0400

    Fix AutoMod AInv

--- source/utility/AutoModeration.js
+++ source/utility/AutoModeration.js
@@ -12,7 +12,7 @@ module.exports = class {
                 const embedMatch = this.client.functions.inviteCheck(inspect(response.message.embeds, { depth: 4 }));
 
                 if (contentMatch || embedMatch) {
-                    this.client.eventsManager.guildInvitePosted(response.message.guild, response.message, response.message.author);
+                    this.client.emit("guildInvitePosted", response.message.guild, response.message, response.message.author);
                     response.message.delete().then(() => {
                         response.error(`This server prohibits invites from being sent. Your message has been deleted.`);
                     });
