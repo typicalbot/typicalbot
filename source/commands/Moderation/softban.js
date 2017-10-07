a308a725391c06730aa7208c060b4209b9dd5da2
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-07 08:36:24 -0400

    Hopefully fix softban -> ban log being created

--- source/commands/Moderation/softban.js
+++ source/commands/Moderation/softban.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
         const args = /softban\s+(?:<@!?)?(\d{17,20})>?(?:\s+(\d+))?(?:\s+(.+))?/i.exec(message.content);
         if (!args) return response.usage(this);
 
-        const user = args[1], days = args[2], reason = args[3];
+        const user = args[1], days = args[2] || 2, reason = args[3];
 
         this.client.users.fetch(user).then(async cachedUser => {
             const member = await message.guild.members.fetch(cachedUser);
@@ -23,14 +23,14 @@ module.exports = class extends Command {
             if (message.member.highestRole.position <= member.highestRole.position) return response.error(`You cannot softban a user with either the same or higher highest role.`);
             if (!member.bannable) return response.error(`In order to complete the request, I need the **BAN_MEMBERS** permission and my highest role needs to be higher than the requested user's highest role.`);
 
+            this.client.softbanCache.set(user.id || user, user.id || user);
+
             member.ban({ days }).then(actioned => {
                 setTimeout(() => {
                     message.guild.unban(actioned.id).then(async () => {
                         if (message.guild.settings.logs.moderation) {
                             const log = { "action": "softban", "user": actioned.user, "moderator": message.author };
                             if (reason) Object.assign(log, { reason });
-
-                            this.client.softbanCache.set(actioned.user.id || actioned.user, log);
 
                             await this.client.modlogsManager.createLog(message.guild, log);
 
