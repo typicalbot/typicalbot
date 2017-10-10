204fce6262d9c7f3cc00ac1a387db4a50d0abe40
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-10 16:02:06 -0400

    Restart option

--- source/commands/Staff/update.js
+++ source/commands/Staff/update.js
@@ -11,13 +11,27 @@ module.exports = class extends Command {
     }
 
     execute(message, response, permissionLevel) {
+        const restart = /update\s+(?:-r|--restart)/.test(message.content);
+
         exec("git pull", { cwd: join(__dirname, "..", "..", "..") }, (err, stdout, stderr) => {
             if (err) return console.error(err);
 
-            const embed = response.buildEmbed().setTitle("TypicalBot Git-Pull").setFooter("TypicalBot", "https://typicalbot.com/images/icon.png").setColor(0x00adff);
+            const embed = response.buildEmbed().setTitle("TypicalBot Updater").setFooter("TypicalBot", "https://typicalbot.com/images/icon.png").setColor(0x00adff);
 
             if (stdout) embed.addField("» STDOUT", stdout.toString().substring(0, 1024));
             if (stderr) embed.addField("» STDERR", stderr.toString().substring(0, 1024));
+
+            if (restart) embed.addBlankField().addField("Restart", "Restarting now...");
+
+            exec(`pm2 restart ${
+                this.client.build === "stable" ?
+                    "TB" : this.client.build === "prime" ?
+                        "TBP" : this.client.build === "beta" ?
+                            "TBB" : this.client.build === "development" ?
+                                "TBD" : null }`,
+            (err, stdout, stderr) => {
+                if (err) return console.error(err);
+            });
 
             embed.send();
         });
