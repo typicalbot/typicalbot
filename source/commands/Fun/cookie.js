d895479ba21a1f3f1fafc1fa000746824a3439fc
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-21 13:47:34 -0400

    Add :cookie: back

---
+++ source/commands/Fun/cookie.js
@@ -0,0 +1,17 @@
+const Command = require("../../structures/Command");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: "Give another user a cookie or keep them all for yourself.",
+            usage: "cookie [@user]"
+        });
+    }
+
+    execute(message, response, permissionLevel) {
+        const user = message.mentions.users.first();
+
+        if (!user || user.id === message.author.id) return response.send(`${message.author} decided to keep all of the cookies for himself! What a jerk! :angry:`);
+        response.send(`${message.author} just gave ${user} a cookie. :cookie:`);
+    }
+};
