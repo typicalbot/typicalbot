fdb0ea87642666219270f654acc4ec4c397be9cf
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-21 19:18:20 -0400

    Update ShardingMaster

--- index.js
+++ index.js
@@ -1,108 +1,30 @@
-const { fork } = require("child_process");
+const { Collection } = require("discord.js");
+
 const { build } = require("./package");
 const config = require(`./configs/${build}`);
-const Discord = require("discord.js");
 
 const SHARD_COUNT   = config.shards;
 const CLIENT_TOKEN  = config.token;
 
-class Shard extends fork {
-    constructor(master, id) {
-        super(`${__dirname}/source/client.js`, [], { env: { SHARD_ID: id, SHARD_COUNT, CLIENT_TOKEN, CLIENT_BUILD: build } });
+const Shard = require("./structures/Shard");
 
-        this.id = id;
-
-        this.stats = {};
-
-        this.commands = Array(60).fill(0);
-
-        this.master = master;
-
-        this.status = null;
-        this.uptime = null;
-
-        this.on("message", message => {
-            if (message.type === "stat" || message.type === "stats") {
-                this.master.changeStats(this.id, message.data);
-            } else if (message.type === "commands") {
-                this.commands = message.data.commands;
-            } else if (message.type === "status") {
-                this.status = message.data.status;
-                this.uptime = message.data.uptime;
-            } else if (message.type === "masterrequest") {
-                const r = this.master.pendingRequests.get(message.data.id);
-                if (!r) return;
-                r.callback(message);
-            } else if (message.type === "donors") {
-                this.master.donorData = message.data;
-                this.master.transmit(message.type, message.data);
-            } else {
-                this.master.transmit(message.type, message.data);
-            }
-        });
-    }
-}
-
-new class {
+class ShardingMaster {
     constructor() {
-        this.shards = new Discord.Collection();
+        this.shards = new Collection();
+        
         this.stats = [];
-        this.pendingRequests = new Discord.Collection();
-
-        this.donorData = [];
 
         this.init();
     }
 
-    staff(userid) {
-        if (
-            userid === config.creator ||
-            config.management[userid] ||
-            userid === config.devhelp ||
-            config.staff[userid] ||
-            config.support[userid]
-        ) return true;
-        return false;
-    }
-
-    globalRequest(request, data) {
-        return new Promise((resolve, reject) => {
-            const id = Math.random();
-
-            const timeout = setTimeout(() => { this.pendingRequests.delete(id); return reject("Timed Out"); }, 100);
-
-            const callback = (response) => {
-                clearTimeout(timeout);
-                this.pendingRequests.delete(id);
-                return resolve(response.data);
-            };
-
-            this.pendingRequests.set(id, { callback, timeout });
-
-            this.transmit(request, Object.assign(data, { id }));
-        });
-    }
-
     create(id) {
-        this.shards.set(id, new Shard(this, id));
+        this.shards.set(
+            id,
+            new Shard(this, id, SHARD_COUNT, CLIENT_TOKEN, build)
+        );
     }
 
-    changeStats(shard, data) {
-        Object.keys(data).map(key => this.shards.get(shard).stats[key] = data[key]);
-        this.relayStats();
-    }
-
-    relayStats() {
-        const data = {};
-        this.shards.forEach(shard => {
-            Object.keys(shard.stats).forEach(key => {
-                data[key] ? data[key] += shard.stats[key] : data[key] = shard.stats[key];
-            });
-        });
-        this.transmit("stats", data);
-    }
-
-    transmit(type, data) {
+    broadcast(type, data) {
         this.shards.forEach(shard => {
             shard.send({
                 type,
@@ -111,9 +33,28 @@ new class {
         });
     }
 
+    updateStats(shard, data) {
+        Object.keys(data).map(key => this.shards.get(shard).stats[key] = data[key]);
+
+        const newData = {};
+        
+        this.shards.forEach(shard => {
+            Object.keys(shard.stats).forEach(key => {
+                newData[key] ? newData[key] += shard.stats[key] : newData[key] = shard.stats[key];
+            });
+        });
+
+        this.broadcast("stats", newData);
+    }
+
     init() {
         for (let s = 0; s < SHARD_COUNT; s++) {
-            setTimeout(this.create.bind(this), (9000 * s), s);
+            setTimeout(
+                () => this.create(this),
+                (9000 * s), s
+            );
         }
     }
-};
+}
+
+new ShardingMaster();
---
+++ structures/Shard.js
@@ -0,0 +1,24 @@
+const { fork } = require("child_process");
+
+class Shard extends fork {
+    constructor(master, SHARD_ID, SHARD_COUNT, CLIENT_TOKEN, CLIENT_BUILD) {
+        super(`${__dirname}/source/client.js`, [], { env: { SHARD_ID, SHARD_COUNT, CLIENT_TOKEN, CLIENT_BUILD } });
+
+        this.master = master;
+
+        this.id = SHARD_ID;
+
+        this.stats = {};
+
+        this.on("message", message => {
+            const { type , data } = message;
+            if (type === "stats") {
+                this.master.updateStats(this.id, message.data);
+            } else {
+                this.master.broadcast(message.type, message.data);
+            }
+        });
+    }
+}
+
+module.exports = Shard;
