ac68d92c125a53fc08e8762003776e7b5b30dc2b
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-27 20:40:09 -0400

    Send before kick

--- source/commands/Moderation/kick.js
+++ source/commands/Moderation/kick.js
@@ -23,16 +23,16 @@ module.exports = class extends Command {
             if (message.member.highestRole.position <= member.highestRole.position && (permissionLevel.level !== 4 && permissionLevel.level < 9))  return response.error(`You cannot kick a user with either the same or higher highest role.`);
             if (!member.kickable) return response.error(`In order to complete the request, I need the **KICK_MEMBERS** permission and my highest role needs to be higher than the requested user's highest role.`);
 
+            const embed = cachedUser.buildEmbed().setColor(0xff0000).setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setTitle("TypicalBot Alert System").setDescription(`You have been kicked from **${message.guild.name}**.`).addField("» Moderator", message.author.tag);
+            if (reason) embed.addField("» Reason", reason);
+            embed.send().catch(err => { return; });
+            
             member.kick().then(async actioned => {
                 if (message.guild.settings.logs.moderation) {
                     const log = { "action": "kick", "user": actioned.user, "moderator": message.author };
                     if (reason) Object.assign(log, { reason });
 
                     await this.client.modlogsManager.createLog(message.guild, log);
-
-                    const embed = cachedUser.buildEmbed().setColor(0xff0000).setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setTitle("TypicalBot Alert System").setDescription(`You have been kicked from **${message.guild.name}**.`).addField("» Moderator", message.author.tag);
-                    if (reason) embed.addField("» Reason", reason);
-                    embed.send().catch(err => { return; });
 
                     response.success(`Successfully kicked user \`${actioned.user.tag}\`.`);
                 } else return response.success(`Successfully kicked user **${actioned.user.tag}**.`);
