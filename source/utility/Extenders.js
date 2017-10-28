967fa5b2c49edf45023eea012d3dae6533f3e793
HyperCoder <bryan.pikaardjr@outlook.com>
2017-10-27 20:33:59 -0400

    Add buildEmbed to User

--- source/utility/Extenders.js
+++ source/utility/Extenders.js
@@ -1,10 +1,11 @@
-const { MessageEmbed, TextChannel, DMChannel } = require("discord.js");
+const { MessageEmbed, TextChannel, DMChannel, User } = require("discord.js");
 
 MessageEmbed.prototype.send = function(content) {
     if (!this.sendToChannel || !(this.sendToChannel instanceof TextChannel || this.sendToChannel instanceof DMChannel)) return Promise.reject("Embed not created in a channel");
     return this.sendToChannel.send(content || "", { embed: this });
 };
 
-TextChannel.prototype.buildEmbed = DMChannel.prototype.buildEmbed = function() {
+TextChannel.prototype.buildEmbed = User.prototype.buildEmbed = DMChannel.prototype.buildEmbed = function() {
     return Object.defineProperty(new MessageEmbed(), "sendToChannel", { value: this });
 };
+
