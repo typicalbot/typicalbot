bbae3fc83f2713fa5cf9afb6d6d2a2e246cb7e18
HyperCoder <bryan.pikaardjr@outlook.com>
2017-11-04 19:08:54 -0400

    Ignore & Unignore

---
+++ source/commands/Moderation/ignore.js
@@ -0,0 +1,26 @@
+const Command = require("../../structures/Command");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: "Makes the bot ignore commands or invites in a channel.",
+            usage: "ignore ['commands'|'invites']",
+            mode: "strict",
+            permission: 3
+        });
+    }
+
+    execute(message, response, permissionLevel) {
+        const args = /ignore\s+(channels|invites)/i.exec(message.content);
+        if (!args) return response.usage(this);
+
+        const commands = args[1] === "commands", invites = args[1] === "invites";
+
+        if (commands && message.guild.settings.ignored.commands.includes(message.channel.id)) return response.error("This channel is already ignoring commands.");
+        if (invites && message.guild.settings.ignored.commands.includes(message.channel.id)) return response.error("This channel is already ignoring invites.");
+
+        const newArray = message.guild.settings.ignored[commands ? "commands" : "invites"].push(message.channel.id);
+
+        this.client.settings.update(message.guild.id, { ignored: { [commands ? "commands" : "invites"]: newArray }}).then(() => response.success("Setting successfully updated."));
+    }
+};
