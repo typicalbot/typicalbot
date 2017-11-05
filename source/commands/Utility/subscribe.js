8e93a6ec64ddc5ac594b71adbb6d3463f2eafd70
HyperCoder <bryan.pikaardjr@outlook.com>
2017-11-05 09:44:48 -0500

    Set up subscriber role

--- source/commands/Utility/subscribe.js
+++ source/commands/Utility/subscribe.js
@@ -3,27 +3,31 @@ const Command = require("../../structures/Command");
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            description: "Subscribe to TypicalBot's announcements.",
+            description: "Gain the server's subscriber role.",
             usage: "subscribe",
             mode: "strict"
         });
     }
 
     execute(message, response, permissionLevel) {
-        if (message.guild.id !== "163038706117115906") return response.error(`You must be in TypicalBot's Lounge in order to use this command.`);
+        const role = message.guild.settings.subscriber ? message.guild.roles.get(message.guild.settings.subscriber) : null;
+        if (message.guild.id !== "163038706117115906") role = message.guild.roles.find("name", "Subscriber");
 
-        const Role = message.guild.roles.find("name", "Subscriber");
+        if (!role) return response.error("No subscriber role is set up for this server.");
 
         message.member.addRole(Role).then(() => {
-            response.reply("You are now subscribed to TypicalBot's announcements!");
+            response.reply("You are now subscribed!");
         });
     }
 
     embedExecute(message, response, permissionLevel) {
-        if (message.guild.id !== "163038706117115906") return response.buildEmbed()
+        const role = message.guild.settings.subscriber ? message.guild.roles.get(message.guild.settings.subscriber) : null;
+        if (message.guild.id !== "163038706117115906") role = message.guild.roles.find("name", "Subscriber");
+
+        if (!role) return response.buildEmbed()
             .setColor(0xFF0000)
             .setTitle("Error")
-            .setDescription(`You must be in TypicalBot's Lounge in order to use this command.`)
+            .setDescription(`No subscriber role is set up for this server.`)
             .setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png")
             .setTimestamp()
             .send();
@@ -34,7 +38,7 @@ module.exports = class extends Command {
             response.buildEmbed()
                 .setColor(0x00adff)
                 .setTitle("Success")
-                .setDescription("You are now subscribed to TypicalBot's announcements!")
+                .setDescription("You are now subscribed!")
                 .setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png")
                 .setTimestamp()
                 .send();
