a85e520e597d2717f574d5ebceeb2ebe3e864600
HyperCoder <bryan.pikaardjr@outlook.com>
2017-11-11 13:18:26 -0500

    Add extendables

--- source/utility/Extenders.js
+++ source/utility/Extenders.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, TextChannel, DMChannel, User } = require("discord.js");
+const { MessageEmbed, TextChannel, DMChannel, User, Message } = require("discord.js");
 
 MessageEmbed.prototype.send = function(content) {
     if (!this.sendToChannel || !(this.sendToChannel instanceof TextChannel || this.sendToChannel instanceof User || this.sendToChannel instanceof DMChannel)) return Promise.reject("Embed not created in a channel");
@@ -9,3 +9,28 @@ TextChannel.prototype.buildEmbed = User.prototype.buildEmbed = DMChannel.prototy
     return Object.defineProperty(new MessageEmbed(), "sendToChannel", { value: this });
 };
 
+Message.prototype.send = function(content, embed) {
+    const options = {};
+    if (embed) Object.defineProperty(options, { embed });
+    
+    this.channel.send(content, options);
+}
+
+Message.prototype.reply = function(content, embed) {
+    return this.send(`${this.author} | ${content}`, embed);
+}
+
+Message.prototype.success = function(content, embed) {
+    return this.send(`${this.author} | ✓ | ${content}`, embed);
+}
+
+Message.prototype.error = function(content, embed) {
+    return this.send(`${this.author} | \\❌ | ${content}`, embed);
+}
+
+Message.prototype.dm = function(content, embed) {
+    const options = {};
+    if (embed) Object.defineProperty(options, { embed });
+    
+    this.author.send(content, options);
+}
