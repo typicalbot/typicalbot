2d05ffbc355cd00e2401b44db9bb195733c6ba6f
HyperCoder <bryan.pikaardjr@outlook.com>
2017-11-18 08:48:34 -0500

    Add to access level error

--- source/events/message.js
+++ source/events/message.js
@@ -41,7 +41,7 @@ class New extends Event {
             if (!command) return;
 
             const accessLevel = this.client.functions.fetchAccess(message.guild);
-            if (command.access && accessLevel.level < command.access) return message.error(`The server owner's access level is too low to execute that command. The command requires an access level of ${command.access}, but the owner only has a level of ${accessLevel.level} (${accessLevel.title}).`);
+            if (command.access && accessLevel.level < command.access) return message.error(`The server owner's access level is too low to execute that command. The command requires an access level of ${command.access}, but the owner only has a level of ${accessLevel.level} (${accessLevel.title}). The owner can raise their access level by donating $5 or more to TypicalBot.`);
 
             const mode = command.mode || "free";
             if (message.author.id !== this.client.config.owner && message.author.id !== message.guild.ownerID) if (settings.mode === "lite" && mode === "free" || settings.mode === "strict" && (mode === "free" || mode === "lite")) return message.error(`That command is not enabled on this server.`);
