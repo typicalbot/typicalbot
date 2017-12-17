f32678092258ff06a35ce569b9c35656acbf9a45
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-17 15:01:20 -0500

    ^

--- ipc/app.js
+++ ipc/app.js
@@ -28,7 +28,8 @@ class IPC extends express {
 
             this.master.globalRequest("guildData", { guild }).then(data => {
                 return res.status(200).json(data);
-            }).catch(() => {
+            }).catch(err => {
+                console.log(err);
                 return res.status(500).json({ "message": "Request Timed Out" });
             });
         });
