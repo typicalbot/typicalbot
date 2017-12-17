1d95fd732f594654b8f07fa723ec441cc103e446
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-16 23:49:55 -0500

    ^

--- source/managers/Audio.js
+++ source/managers/Audio.js
@@ -12,26 +12,26 @@ module.exports = class {
         if (!channel.joinable) throw "I require joining permissions to use music features.";
         if (!channel.speakable) throw "I require speaking permissions to use music features.";
 
-        return channel.join().then(connection => {
-            this.client.emit("voiceConnectionUpdate", connection);
+        const connection = await channel.join();
+            
+        this.client.emit("voiceConnectionUpdate", connection);
 
-            Object.defineProperty(connection, "guildStream", { value: new Stream(this.client, connection) });
+        Object.defineProperty(connection, "guildStream", { value: new Stream(this.client, connection) });
 
-            return connection;
-        }).catch(err => { throw err; });
+        return connection;
     }
 
     async stream(message, video) {
         if (!this.client.audioUtility.withinLimit(message, video)) throw `The video you are trying to play is too long. The maximum video length is ${this.client.functions.convertTime(this.client.functions.length(message.guild.settings.lengthLimit * 1000 || 1800 * 1000))}.`;
 
-        Object.defineProperty(video, "requestor", { value: message });
+        Object.defineProperty(video, "requester", { value: message });
 
         if (message.guild.voiceConnection) {
             if (!message.member.voiceChannel || message.member.voiceChannel.id !== message.guild.voiceConnection.channel.id) throw "You must be in the same voice channel to request a video to be played.";
             return message.guild.voiceConnection.guildStream.addQueue(video);
         }
 
-        const connection = await this.connect(message).catch(err => { throw err; });
+        const connection = await this.connect(message);
 
         connection.guildStream.play(video);
     }
