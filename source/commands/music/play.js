872bbe72409aad3a2c99bc570b96ea5a991139f9
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-17 09:33:35 -0500

    ^

--- source/commands/music/play.js
+++ source/commands/music/play.js
@@ -13,20 +13,22 @@ module.exports = class extends Command {
     execute(message, parameters, permissionLevel) {
         if (!this.client.audioUtility.hasPermissions(message, this)) return;
 
-        const args = /(?:(?:https?:\/\/www\.youtube\.com\/playlist\?list=(.+))|(?:https?:\/\/)?(?:(?:www|m)\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+))/i.exec(parameters);
-        if (!args) return message.error(this.client.functions.error("usage", this));
+        const match = /(.+)/i.exec(parameters);
+        if (!match) return message.error(this.client.functions.error("usage", this)); 
 
-        if (args[1]) {
+        const args = /(?:(?:https?:\/\/www\.youtube\.com\/playlist\?list=(.+))|(?:https?:\/\/)?(?:(?:www|m)\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+))/i.exec(parameters);
+        
+        if (args && args[1]) {
             this.client.audioManager.stream(message, args[1], true).catch(err => message.error(err));
-        } else if (args[2]) {
+        } else if (args && args[2]) {
             this.client.audioUtility.fetchInfo(args[2]).then(videoInfo => {
                 videoInfo.url = args[2];
 
                 this.client.audioManager.stream(message, videoInfo).catch(err => message.error(err));
             }).catch(err => message.error(`Information cannot be fetched from that song. Please try another url.`));
         } else {
-            this.client.audioUtility.search(message.guild.settings, args[2]).then(results => {
-                if (!results.length) return message.reply(`No results were found for the query **${args[2]}**.`);
+            this.client.audioUtility.search(message.guild.settings, match[1]).then(results => {
+                if (!results.length) return message.reply(`No results were found for the query **${match[1]}**.`);
                 const video = results[0];
 
                 this.client.audioUtility.fetchInfo(video.url).then(videoInfo => {
