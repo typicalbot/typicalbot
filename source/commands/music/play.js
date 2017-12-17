88f2b577369386b738f8707d65def58a004e1fd1
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-17 09:40:18 -0500

    ^

--- source/commands/music/play.js
+++ source/commands/music/play.js
@@ -19,12 +19,12 @@ module.exports = class extends Command {
         const args = /(?:(?:https?:\/\/www\.youtube\.com\/playlist\?list=(.+))|(?:https?:\/\/)?(?:(?:www|m)\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+))/i.exec(parameters);
         
         if (args && args[1]) {
-            this.client.audioManager.stream(message, args[1], true).catch(err => message.error(err.stack));
+            this.client.audioManager.stream(message, args[1], true).catch(err => message.error(err.stack || err));
         } else if (args && args[2]) {
             this.client.audioUtility.fetchInfo(args[2]).then(videoInfo => {
                 videoInfo.url = args[2];
 
-                this.client.audioManager.stream(message, videoInfo).catch(err => message.error(err));
+                this.client.audioManager.stream(message, videoInfo).catch(err => message.error(err.stack || err));
             }).catch(err => message.error(`Information cannot be fetched from that song. Please try another url.`));
         } else {
             this.client.audioUtility.search(message.guild.settings, match[1]).then(results => {
@@ -34,7 +34,7 @@ module.exports = class extends Command {
                 this.client.audioUtility.fetchInfo(video.url).then(videoInfo => {
                     videoInfo.url = video.url;
 
-                    this.client.audioManager.stream(message, videoInfo).catch(err => message.error(err));
+                    this.client.audioManager.stream(message, videoInfo).catch(err => message.error(err.stack || err));
                 }).catch(err => message.error(`Information cannot be fetched from that song. Please try another song name.${message.author.id === "105408136285818880" ? `\n\n\`\`\`${err}\`\`\`` : ""}`));
             }).catch(error => message.error(`${this.client.audioUtility.searchError(error)}`));
         }
