d5461498c3ae1ff6ad26740233dc7f9a9be84a75
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-16 23:59:34 -0500

    ^

--- source/commands/music/queue.js
+++ source/commands/music/queue.js
@@ -23,7 +23,7 @@ module.exports = class extends Command {
 
         const list = queue.slice(0, 10);
 
-        const content = list.map(s => `● **${short(s.title)}** (${time(s.length_seconds)}) | Requested by **${s.response.message.author.username}**`).join("\n");
+        const content = list.map(s => `● **${short(s.title)}** (${time(s.length_seconds)}) | Requested by **${s.requester.author.username}**`).join("\n");
         let length = 0; queue.forEach(s => length += Number(s.length_seconds));
 
         message.send(`**__Queue:__** There are **${queue.length}** videos in the queue. The queue will last for **${time(length)}.**\n\n${content}${queue.length > 10 ? `\n*...and ${queue.length - 10} more.*` : ""}\n\n**__Currently Streaming:__** **${short(connection.guildStream.current.title)}** (${time(connection.guildStream.current.length_seconds)}) | Requested by **${connection.guildStream.current.requester.author.username}**`);
