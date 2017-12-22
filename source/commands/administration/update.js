6a274c84db41316c0afe7dc5cbce9975e9d87ef5
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-22 16:26:16 -0500

    Update update

--- source/commands/administration/update.js
+++ source/commands/administration/update.js
@@ -13,41 +13,43 @@ module.exports = class extends Command {
     execute(message, parameters, permissionLevel) {
         const restart = /update\s+(?:-r|--restart)/.test(message.content);
 
-        message.reply("Are you sure?");
+        const path = process.cwd();
+
+        exec("git pull", { cwd: path }, (err, stdout, stderr) => {
+            if (err) return console.error(err);
+
+            const embed = message.buildEmbed().setTitle("TypicalBot Updater").setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setColor(0x00adff);
+
+            if (stdout) embed.addField("» STDOUT", stdout.toString().substring(0, 1024));
+            if (stderr) embed.addField("» STDERR", stderr.toString().substring(0, 1024));
+
+            if (restart) {
+                message.reply("Are you sure?");
+                
+                message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 10000, errors: ["time"] })
+                    .then(messages => {
+                        const msg = messages.first();
         
-        message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 10000, errors: ["time"] })
-            .then(messages => {
-                const msg = messages.first();
+                        if (msg.content.toLowerCase() !== "y" && msg.content.toLowerCase() !== "yes") return message.reply("Canceling.");
 
-                if (msg.content.toLowerCase() !== "y" && msg.content.toLowerCase() !== "yes") return message.reply("Canceling.");
+                        embed.addField("\u200B", "Restarting now...");
 
-                const path = process.cwd();
-
-                exec("git pull", { cwd: path }, (err, stdout, stderr) => {
-                    if (err) return console.error(err);
-
-                    const embed = message.buildEmbed().setTitle("TypicalBot Updater").setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setColor(0x00adff);
-
-                    if (stdout) embed.addField("» STDOUT", stdout.toString().substring(0, 1024));
-                    if (stderr) embed.addField("» STDERR", stderr.toString().substring(0, 1024));
-
-                    if (restart) embed.addField("\u200B", "Restarting now...");
-
-                    if (restart) exec(`pm2 restart ${
-                        this.client.build === "stable" ?
-                            "TB" : this.client.build === "prime" ?
-                                "TBP" : this.client.build === "beta" ?
-                                    "TBB" : this.client.build === "development" ?
-                                        "TBD" : null }`,
-                    { cwd: path, env: { HOME: "/home/hypercoder" } },
-                    (err, stdout, stderr) => {
-                        if (err) return console.error(err);
+                        exec(`pm2 restart ${
+                            this.client.build === "stable" ?
+                                "TB" : this.client.build === "prime" ?
+                                    "TBP" : this.client.build === "beta" ?
+                                        "TBB" : this.client.build === "development" ?
+                                            "TBD" : null}`,
+                        { cwd: path, env: { HOME: "/home/hypercoder" } },
+                        (err, stdout, stderr) => {
+                            if (err) return console.error(err);
+                        });
+                    }).catch(err => {
+                        message.error("An answer was not given.");
                     });
+            }
 
-                    embed.send();
-                });
-            }).catch(err => {
-                message.error("An answer was not given.");
-            })
+            embed.send();
+        });
     }
 };
