9130dcfe0eff61a8377c9b5bb85c91341f29d2d8
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-22 15:59:26 -0500

    Awaiting "y" or "yes" so I don't keep restarting TB

--- source/commands/administration/update.js
+++ source/commands/administration/update.js
@@ -13,30 +13,39 @@ module.exports = class extends Command {
     execute(message, parameters, permissionLevel) {
         const restart = /update\s+(?:-r|--restart)/.test(message.content);
 
-        const path = process.cwd();
+        message.channel.awaitMessages(m => m.author.id === message.author.id, { max: 1, time: 10000, errors: ["time"] })
+            .then(messages => {
+                const msg = messages.first();
 
-        exec("git pull", { cwd: path }, (err, stdout, stderr) => {
-            if (err) return console.error(err);
+                if (msg.content.toLowerCase() !== "y" && msg.content.toLowerCase() !== "yes") return message.reply("Canceling.");
 
-            const embed = message.buildEmbed().setTitle("TypicalBot Updater").setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setColor(0x00adff);
+                const path = process.cwd();
 
-            if (stdout) embed.addField("» STDOUT", stdout.toString().substring(0, 1024));
-            if (stderr) embed.addField("» STDERR", stderr.toString().substring(0, 1024));
+                exec("git pull", { cwd: path }, (err, stdout, stderr) => {
+                    if (err) return console.error(err);
 
-            if (restart) embed.addField("\u200B", "Restarting now...");
+                    const embed = message.buildEmbed().setTitle("TypicalBot Updater").setFooter("TypicalBot", "https://typicalbot.com/x/images/icon.png").setColor(0x00adff);
 
-            if (restart) exec(`pm2 restart ${
-                this.client.build === "stable" ?
-                    "TB" : this.client.build === "prime" ?
-                        "TBP" : this.client.build === "beta" ?
-                            "TBB" : this.client.build === "development" ?
-                                "TBD" : null }`,
-            { cwd: path, env: { HOME: "/home/hypercoder" } },
-            (err, stdout, stderr) => {
-                if (err) return console.error(err);
-            });
+                    if (stdout) embed.addField("» STDOUT", stdout.toString().substring(0, 1024));
+                    if (stderr) embed.addField("» STDERR", stderr.toString().substring(0, 1024));
 
-            embed.send();
-        });
+                    if (restart) embed.addField("\u200B", "Restarting now...");
+
+                    if (restart) exec(`pm2 restart ${
+                        this.client.build === "stable" ?
+                            "TB" : this.client.build === "prime" ?
+                                "TBP" : this.client.build === "beta" ?
+                                    "TBB" : this.client.build === "development" ?
+                                        "TBD" : null }`,
+                    { cwd: path, env: { HOME: "/home/hypercoder" } },
+                    (err, stdout, stderr) => {
+                        if (err) return console.error(err);
+                    });
+
+                    embed.send();
+                });
+            }).catch(err => {
+                message.error("An answer was not given.");
+            })
     }
 };
