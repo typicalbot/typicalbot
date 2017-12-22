6ddf41447e94afd5af521a1a2130934badb759e0
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-22 16:46:23 -0500

    ^

--- source/events/ready.js
+++ source/events/ready.js
@@ -23,7 +23,7 @@ class New extends Event {
         }, 1000 * 60 * 5);
 
         setInterval(() => {
-            this.client.voiceConnections.filter(c => c.channel.members.filter(m => !m.user.bot).size === 0).forEach(c => c.guildStream.destroy());
+            this.client.voiceConnections.filter(c => c.channel.members.filter(m => !m.user.bot).size === 0).forEach(c => c.guildStream.end());
         }, 1000 * 30);
     }
 }
