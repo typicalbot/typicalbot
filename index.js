d86ee9000c08a569230cba0d01dab70e7e8de8e1
HyperCoder <bryan.pikaardjr@outlook.com>
2017-12-31 10:19:49 -0500

    Changes & Hopeful Bug Fixes

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ require.extensions['.txt'] = function (module, filename) { module.exports = requ
 
 const { Collection } = require("discord.js");
 
+const IPC = require("./ipc/app");
+
 const build = require("./build");
-console.log(build);
 const config = require(`./configs/${build}`);
 
 const SHARD_COUNT   = config.shards;
@@ -14,12 +15,36 @@ const Shard = require("./structures/Shard");
 class ShardingMaster extends Collection {
     constructor() {
         super();
-        
+
         this.stats = [];
+
+        if (build === "development") this.ipc = new IPC(this);
+
+        this.pendingRequests = new Collection();
 
         for (let s = 0; s < SHARD_COUNT; s++) {
             setTimeout(() => this.set(s, new Shard(this, s, SHARD_COUNT, CLIENT_TOKEN, build)), (9000 * s));
         }
+    }
+
+    globalRequest(request, data) {
+        return new Promise((resolve, reject) => {
+            const id = Math.random();
+
+            const timeout = setTimeout(() => { this.pendingRequests.delete(id); return reject("Timed Out"); }, 100);
+
+            const callback = (response) => {
+                clearTimeout(timeout);
+
+                this.pendingRequests.delete(id);
+
+                return resolve(response.data);
+            };
+
+            this.pendingRequests.set(id, { callback, timeout });
+
+            this.broadcast(request, Object.assign(data, { id }));
+        });
     }
 
     broadcast(type, data) {
@@ -35,12 +60,14 @@ class ShardingMaster extends Collection {
         Object.keys(data).map(key => this.get(shard).stats[key] = data[key]);
 
         const newData = {};
-        
+
         this.forEach(shard => {
             Object.keys(shard.stats).forEach(key => {
                 newData[key] ? newData[key] += shard.stats[key] : newData[key] = shard.stats[key];
             });
         });
+
+        this.stats = newData;
 
         this.broadcast("stats", newData);
     }
---
+++ ipc/app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = 5000;
+
+class IPC extends express {
+    constructor(master) {
+        super();
+
+        this.master = master;
+
+        this.use(bodyParser.json());
+
+        /*
+                                                           - - - - - - - - - -
+
+                                                                ENDPOINTS
+
+                                                           - - - - - - - - - -
+        */
+
+        this.get("/stats", (req, res, next) => {
+            res.json(master.stats);
+        });
+
+        this.get("/guilds/:guildid", (req, res, next) => {
+            const guild = req.params.guildid;
+
+            this.master.globalRequest("guildData", { guild }).then(data => {
+                return res.status(200).json(data);
+            }).catch(err => {
+                return res.status(500).json({ "message": "Request Timed Out", "error": err });
+            });
+        });
+
+        this.post("/guilds/:guildid/leave", (req, res, next) => {
+            const guild = req.params.guildid;
+
+            this.master.globalRequest("leaveGuild", { guild }).then(data => {
+                return res.status(200);
+            }).catch(err => {
+                return res.status(500).json({ "message": "Request Timed Out", "error": err  });
+            });
+        });
+
+        this.get("/guilds/:guildid/users/:userid", (req, res, next) => {
+            const guild = req.params.guildid;
+            const user = req.params.userid;
+
+            this.master.globalRequest("userData", { guild, user }).then(data => {
+                if (data.permissions.level < 2) return res.redirect("/access-denied");
+
+                return res.status(200).json(data);
+            }).catch(err => {
+                return res.status(500).json({ "message": "Request Timed Out", "error": err  });
+            });
+        });
+
+        /*
+                                                           - - - - - - - - - -
+
+                                                                INIT SERVER
+
+                                                           - - - - - - - - - -
+        */
+
+        this.listen(port, () => console.log(`Express Server Created | Listening on Port :${port}`));
+    }
+}
+
+module.exports = IPC;
