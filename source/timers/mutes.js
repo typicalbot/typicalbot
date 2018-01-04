b22c742fa2cc3f546f4f8c9d45c6761e17614c38
HyperCoder <bryan.pikaardjr@outlook.com>
2018-01-04 17:18:14 -0500

    Update mutes.js timer

--- source/timers/mutes.js
+++ source/timers/mutes.js
@@ -25,8 +25,8 @@ class New extends Timer {
     }
 
     async execute(timer) {
-        const guild = this.client.guilds.get(timer.guild); if (!guild) return;
-        const member = await guild.members.fetch(timer.member); if (!member) return;
+        const guild = this.client.guilds.get(timer.guild); if (!guild) return console.log("No guild");
+        const member = await guild.members.fetch(timer.member); if (!member) return console.log("No member");
 
         const settings = await guild.fetchSettings();
 
