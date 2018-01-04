343073d30fd81679b615cf53d1db1e340c25b97c
HyperCoder <bryan.pikaardjr@outlook.com>
2018-01-04 15:41:52 -0500

    Video Structure

---
+++ source/structures/Video.js
@@ -0,0 +1,43 @@
+const ytdl = require("ytdl-core");
+const SYS = require("simple-youtube-stream");
+const sys = new SYS();
+
+class Video {
+    constructor(url, { title, length_seconds, live_playback }) {
+        this.url = url;
+
+        this.title = title;
+
+        this.length = length_seconds;
+
+        this.live = !!live_playback;
+
+        this.requestor;
+    }
+
+    setRequestor(message) {
+        Object.defineProperty(this, "requestor", { value: message });
+        
+        return this;
+    }
+
+    async validate(url) {
+        const id = /[a-zA-Z0-9-_]{11}$/.exec(url);
+        if (id) url = id;
+        
+        const data = sys.fetchInfo(url).catch(err => { throw err; });
+        return data;
+    }
+
+    async stream() {
+        const validated = await this.validate(this.url).catch(err => { throw err; });
+
+        const options = {};
+        if (!this.live_playback) Object.assign(options, { filter: "audioonly"});
+            
+        const audioStream = ytdl(this.url, options);
+        return audioStream;
+    }
+}
+
+module.exports = Video;
