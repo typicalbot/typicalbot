d840b3bc352f3bffde6f2e8e3610ae89b658cfda
HyperCoder <bryan.pikaardjr@outlook.com>
2018-01-24 22:52:24 -0500

    ^

--- ipc/app.js
+++ ipc/app.js
@@ -34,8 +34,7 @@ class IPC extends express {
         });
 
         this.post("/guilds/:guildid/leave", (req, res, next) => {
-            console.log(req.headersSent, req.get("token"));
-            if (req.get("token") !== this.master.config.apitoken) return res.status(403).json({ "message": "Not Authenticated" });
+            if (req.get("token").replace("'", "") !== this.master.config.apitoken) return res.status(403).json({ "message": "Not Authenticated" });
 
             const guild = req.params.guildid;
 
