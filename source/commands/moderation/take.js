025802bb621a3143268ca39c1de98cbed7c008ca
SirPacker <git@cheezewerks.com>
2018-02-13 21:03:13 -0500

    Update Commands.Give/Take | Add functionality. What did you expect?

--- source/commands/moderation/take.js
+++ source/commands/moderation/take.js
@@ -10,21 +10,37 @@ module.exports = class extends Command {
         });
     }
 
-    execute(message, parameters, permissionLevel) {
-        const args = /(?:(?:<@&)?(\d{17,20})>?|(.+))/i.exec(parameters);
+    async execute(message, parameters, permissionLevel) {
+        const args = /(?:(?:(?:<@!?)?(\d{17,20})>?)|(?:(.+)#(\d{4})))?(?:\s+)?(?:(?:<@&)?(\d{17,20})>?|(.+))/i.exec(parameters);
         if (!args) return message.error(this.client.functions.error("usage", this));
 
-        const role = args[1] ? message.guild.roles.get(args[1]) : args[2] ? message.guild.roles.find(r => r.name.toLowerCase() === args[2].toLowerCase()) : null;
-        if (!role) return message.error("Invalid role. Please make sure your spelling is correct, and that the role actually exists.");
-        if (!role.editable) return message.error(`Insufficient permissions given to the bot. Make sure that the highest role I have is above the role you are attempting to give and that I have the Manage Roles permission.`);
+        const actualUserPermissions = message.member.fetchPermissions(true);
+        const member = await this.client.functions.resolveMember(message, args, false);
 
-        const publicList = message.guild.settings.roles.public;
-        if (!publicList.includes(role.id)) return message.error("The requested role isn't on the list of public roles.");
+        if (member) {
+            if (actualUserPermissions.level < Constants.Permissions.Levels.SERVER_ADMINISTRATOR)
+                return message.error(this.client.functions.error("perms", { permission: Constants.Permissions.Levels.SERVER_ADMINISTRATOR }, actualUserPermissions));
 
-        if (!message.member) return message.error("Apparently you don't exist! I'm not too sure what is causing this, but it's an issue with the Discord.JS library. Sorry!");
+            const role = args[4] ? message.guild.roles.get(args[4]) : args[5] ? message.guild.roles.find(r => r.name.toLowerCase() === args[5].toLowerCase()) : null;
+            if (!role) return message.error("Invalid role. Please make sure your spelling is correct, and that the role actually exists.");
+            if (!role.editable) return message.error(`Insufficient permissions given to the bot. Make sure that the highest role I have is above the role you are attempting to take and that I have the Manage Roles permission.`);
 
-        message.member.roles.remove(role)
-            .then(() => message.reply(`Success.`))
-            .catch(err => message.error(`An error occured while processing that request.`));
+            member.roles.remove(role)
+                .then(() => message.reply(`Success.`))
+                .catch(err => message.error(`An error occured while processing that request.`));
+        } else {
+            const role = args[4] ? message.guild.roles.get(args[4]) : args[5] ? message.guild.roles.find(r => r.name.toLowerCase() === args[5].toLowerCase()) : null;
+            if (!role) return message.error("Invalid role. Please make sure your spelling is correct, and that the role actually exists.");
+            if (!role.editable) return message.error(`Insufficient permissions given to the bot. Make sure that the highest role I have is above the role you are attempting to take and that I have the Manage Roles permission.`);
+
+            const publicList = message.guild.settings.roles.public;
+            if (!publicList.includes(role.id)) return message.error("The requested role isn't on the list of public roles.");
+
+            if (!message.member) return message.error("Apparently you don't exist! I'm not too sure what is causing this, but it's an issue with the Discord.JS library. Sorry!");
+
+            message.member.roles.remove(role)
+                .then(() => message.reply(`Success.`))
+                .catch(err => message.error(`An error occured while processing that request.`));
+        }
     }
 };
