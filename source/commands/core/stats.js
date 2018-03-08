8092d947a104b6f59f15db4bf52a30ada91508fd
HyperCoder <bryan.pikaardjr@outlook.com>
2018-03-08 15:57:11 -0500

    Update stats command

--- source/commands/core/stats.js
+++ source/commands/core/stats.js
@@ -19,6 +19,9 @@ module.exports = class extends Command {
             + `=> Uptime            : ${this.client.functions.convertTime(this.client.uptime)}\n`
             + `=> Servers           : ${this.client.shards.guilds.toLocaleString()} (${this.client.shardCount} Shard${this.client.shardCount > 1 ? "s" : ""})\n`
             + `=> Voice Connections : ${this.client.shards.voiceConnections.toLocaleString()}\n`
+            + `=> CPU               : ${Math.round(loadavg()[0] * 10000) / 100}%\n`
+            + `=> RAM (Used)        : ${this.client.shards.ram_used}MB\n`
+            + `=> RAM (Total)       : ${this.client.shards.ram_total}MB\n`
             + `=> Library           : discord.js\n`
             + `=> Created By        : HyperCoder#2975\n\n`
             + `    This Shard:\n`
@@ -27,8 +30,6 @@ module.exports = class extends Command {
             + `=> Voice Connections : ${this.client.voiceConnections.size.toLocaleString()}\n`
             + `=> Channels          : ${this.client.channels.size.toLocaleString()}\n`
             + `=> Users             : ${this.client.users.size.toLocaleString()}\n`
-            + `    Usage:\n`
-            + `=> CPU               : ${Math.round(loadavg()[0] * 10000) / 100}%\n`
             + `=> RAM (Used)        : ${Math.round(100 * (process.memoryUsage().heapUsed / 1048576)) / 100}MB\n`
             + `=> RAM (Total)       : ${Math.round(100 * (process.memoryUsage().heapTotal / 1048576)) / 100}MB\n`
             + `=> Users             : ${this.client.users.size.toLocaleString()}\n`
