5831a5d05a264d6211531ac4f330785ee20b0461
Tyler0130 <rocket0191@gmail.com>
2018-04-07 15:16:06 -0500

    Improved setup script to create whatever needs to be created

--- setup/init.js
+++ setup/init.js
@@ -4,18 +4,34 @@ const credentials = require(`../config`).database.credentials;
 const r = redb(credentials);
 
 (async function () {
-    if ((await r.dbList()).includes(credentials.db)) throw "Database Exists";
+    if (!(await r.dbList()).includes(credentials.db)) {
+        console.log(`Database \`${credentials.db}\` not found. Creating...`);
+        await r.dbCreate(credentials.db);
+    }
     
-    await r.dbCreate(credentials.db);
-
     const db = r.db(credentials.db);
     
-    await db.tableCreate("guilds");
-    await db.tableCreate("mutes");
-    await db.tableCreate("tasks");
-    await db.tableCreate("donors");
-    await db.tableCreate("partners");
+    if (!(await db.tableList()).includes("guilds")) {
+        console.log("Table `guilds` not found. Creating...");
+        await db.tableCreate("guilds");
+    }
+    if (!(await db.tableList()).includes("mutes")) {
+        console.log("Table `mutes` not found. Creating...");
+        await db.tableCreate("mutes");
+    }
+    if (!(await db.tableList()).includes("tasks")) {
+        console.log("Table `tasks` not found. Creating...");
+        await db.tableCreate("tasks");
+    }
+    if (!(await db.tableList()).includes("donors")) {
+        console.log("Table `donors` not found. Creating...");
+        await db.tableCreate("donors");
+    }
+    if (!(await db.tableList()).includes("partners")) {
+        console.log("Table `partners` not found. Creating...");
+        await db.tableCreate("partners");
+    }
 
-    console.log("Database Built");
+    console.log("The database should be good to go.");
     process.exit();
 })();
