3db467803002b2bf388b0b9e43fb8f236693436d
HyperCoder <bryan.pikaardjr@outlook.com>
2018-04-07 15:50:43 -0400

    Remove unneeded array

--- source/functions/fetchDonors.js
+++ source/functions/fetchDonors.js
@@ -7,7 +7,7 @@ class New extends Function {
 
     async execute() {
         const database = this.client.handlers.database.connection;
-        const data = (await this.client.handlers.database.connection.table("donors")) || [];
+        const data = await this.client.handlers.database.connection.table("donors");
 
         data.forEach(e => {
             if (e.id.length > 5) this.client.caches.donors.set(e.id, e);
