ea8045bff26973deba759dc407477104e843e45a
HyperCoder <bryan.pikaardjr@outlook.com>
2018-05-19 10:17:15 -0400

    Remove Beta Tester Functions

--- source/events/ready.js
+++ source/events/ready.js
@@ -13,15 +13,10 @@ class New extends Event {
         this.client.handlers.process.log(`Client Connected | Shard ${this.client.shardNumber} / ${this.client.shardCount}`);
         this.client.user.setActivity(`Client Started`);
         this.client.handlers.process.transmitStats();
-        this.client.handlers.process.transmit("transmitTesters");
         this.client.functions.fetchDonors();
 
         this.intervals.push(setInterval(() => {
             this.client.user.setActivity(`${this.client.config.prefix}help | ${this.client.shards.guilds} Servers`);
-
-            if (this.client.guilds.has("163038706117115906")) {
-                this.client.functions.transmitTesters();
-            }
         }, 1000 * 60 * 5));
 
         this.intervals.push(setInterval(() => {
