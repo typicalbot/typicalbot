544db49476ef7725fc9716594b3a956057f20911
HyperCoder <bryan.pikaardjr@outlook.com>
2018-06-13 19:54:39 -0400

    Simplification

--- index.js
+++ index.js
@@ -9,30 +9,20 @@ class ShardHandler extends Collection {
 
         this.config = config;
 
-        for (let s = 0; s < config.shards; s++) {
+        for (let s = 0; s < config.shards; s++)
             setTimeout(() => this.set(s, new Shard(this, s, config.shards)), (8000 * s));
-        }
     }
 
     get stats() {
         const data = {};
 
-        this.forEach(shard => {
-            Object.keys(shard.stats).forEach(key => {
-                data[key] ? data[key] += shard.stats[key] : data[key] = shard.stats[key];
-            });
-        });
+        this.forEach(shard => Object.keys(shard.stats).forEach(key => data[key] ? data[key] += shard.stats[key] : data[key] = shard.stats[key]));
 
         return data;
     }
 
     broadcast(event, data) {
-        this.forEach(shard => {
-            shard.send({
-                event,
-                data
-            });
-        });
+        this.forEach(shard => shard.send({ event, data }));
     }
 }
 
