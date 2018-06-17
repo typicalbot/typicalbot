0004609239dc51b96759deb614f4b6f625d949ae
HyperCoder <bryan.pikaardjr@outlook.com>
2018-06-17 13:30:01 -0400

    Error on Rejection

--- src/client/handlers/Process.js
+++ src/client/handlers/Process.js
@@ -1,10 +1,17 @@
+const Raven = require("raven");
+
 class ProcessHandler {
     constructor(client) {
         Object.defineProperty(this, "client", { value: client });
 
         process.on("message", message => this.message(message));
         process.on("uncaughtException", err => this.log(err.stack, true));
-        process.on("unhandledRejection", err => { if (!err) return; this.log(`Uncaught Promise Error:\n${err.stack || JSON.stringify(err) || err}`, true); });
+        process.on("unhandledRejection", err => {
+            if (!err) return;
+            
+            this.log(`Uncaught Promise Error:\n${err.stack || JSON.stringify(err) || err}`, true);
+            Raven.captureException(err);
+        });
     }
 
     async message(message) {
