4cf65f36edca49c67a1a1f50d98d2eee3794fc4d
HyperCoder <bryan.pikaardjr@outlook.com>
2018-06-17 12:53:01 -0400

    Update postStats

--- src/client/functions/postStats.js
+++ src/client/functions/postStats.js
@@ -1,31 +1,31 @@
 const Function = require("../structures/Function");
 const request = require("superagent");
 
-function sendCarbonitex() {
+function sendCarbonitex(client) {
     request.post("https://www.carbonitex.net/discord/data/botdata.php")
         .set("Content-Type", "application/json")
         .send({
-            "shardid": this.client.shardID.toString(),
-            "shardcount": this.client.shardCount.toString(),
-            "servercount": this.client.guilds.size.toString(),
-            "key": this.client.config.apis.carbon
+            "shardid": client.shardID.toString(),
+            "shardcount": client.shardCount.toString(),
+            "servercount": client.guilds.size.toString(),
+            "key": client.config.apis.carbon
         })
         .end((err, res) => {
-            if (err || res.statusCode != 200) this.client.handlers.process.log(`Carbinitex Stats Transfer Failed ${err.body || err}`, true);
+            if (err || res.statusCode != 200) client.handlers.process.log(`Carbinitex Stats Transfer Failed ${err.body || err}`, true);
         });
 }
 
-function sendDiscordBots() {
-    request.post(`https://discordbots.org/api/bots/${this.client.config.id}/stats`)
+function sendDiscordBots(client) {
+    request.post(`https://discordbots.org/api/bots/${client.config.id}/stats`)
         .set("Content-Type", "application/json")
-        .set("Authentication", this.client.config.apis.discordbots)
+        .set("Authentication", client.config.apis.discordbots)
         .send({
-            "shard_id": this.client.shardID.toString(),
-            "shard_count": this.client.shardCount.toString(),
-            "server_count": this.client.guilds.size.toString()
+            "shard_id": client.shardID.toString(),
+            "shard_count": client.shardCount.toString(),
+            "server_count": client.guilds.size.toString()
         })
         .end((err, res) => {
-            if (err || res.statusCode != 200) this.client.handlers.process.log(`DiscordBots Stats Transfer Failed ${err.body || err}`, true);
+            if (err || res.statusCode != 200) client.handlers.process.log(`DiscordBots Stats Transfer Failed ${err.body || err}`, true);
         });
 }
 
@@ -36,10 +36,10 @@ class New extends Function {
 
     execute(provider) {
         if (provider === "a") {
-            sendCarbonitex().bind(this);
-            sendDiscordBots().bind(this);
-        } else if (provider === "c") sendCarbonitex().bind(this);
-        else if (provider === "b") sendDiscordBots().bind(this);
+            sendCarbonitex(this.client);
+            sendDiscordBots(this.client);
+        } else if (provider === "c") sendCarbonitex(this.client);
+        else if (provider === "b") sendDiscordBots(this.client);
     }
 }
 
