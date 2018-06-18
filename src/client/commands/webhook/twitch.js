45d6778ffa07f0b6e5ee0a6c90d1d25773d8d445
HyperCoder <bryan.pikaardjr@outlook.com>
2018-06-18 10:43:21 -0400

    Twitch Subscriptions

--- src/client/commands/webhook/twitch.js
+++ src/client/commands/webhook/twitch.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
     }
 
     async execute(message, parameters, permissionLevel) {
-        const args = /(lookup|subscribe|unsubscribe)(?:\s+(\w+))?/i.exec(parameters);
+        const args = /(lookup|subscribe|unsubscribe)\s+(\w+)/i.exec(parameters);
         if (!args) return message.error(this.client.functions.error("usage", this));
 
         const action = args[1], login = args[2];
@@ -23,15 +23,21 @@ module.exports = class extends Command {
         const actualUserPermissions = await this.client.handlers.permissions.fetch(message.guild, message.author, true);
 
         if (action === "lookup") {
-            if (!login) return message.error(this.client.functions.error("usage", this));
-
             const user = await this.client.handlers.webhooks.twitch.lookup(login);
 
             message.reply(`${user.display_name}: https://www.twitch.tv/${user.login}`);
         } else if (action === "subscribe") {
             if (actualUserPermissions.level < 2) return message.error(this.client.functions.error("perms", { permission: 2 }, actualUserPermissions));
 
-            return message.reply("This command is still being developed.");
+            const user = await this.client.handlers.webhooks.twitch.lookup(login);
+
+            this.client.handlers.webhooks.twitch.addSubscription(message.guild, user.id)
+                .then(() => {
+                    message.reply("Subscribed!");
+                })
+                .catch(err => {
+                    message.error(err);
+                });
         } else if (action === "unsubscribe") {
             if (actualUserPermissions.level < 2) return message.error(this.client.functions.error("perms", { permission: 2 }, actualUserPermissions));
 
--- src/client/handlers/webhooks/Twitch.js
+++ src/client/handlers/webhooks/Twitch.js
@@ -17,12 +17,60 @@ class TwitchWebhookHandler {
         return body.data[0];
     }
 
-    async subscribe(guild, twitchChannel) {
-        snekfetch.post("https://api.twitch.tv/helix/webhooks/hub")
+    async subscribe(id) {
+        await snekfetch
+            .post("https://api.twitch.tv/helix/webhooks/hub")
+            .set("Content-Type", "application/json")
+            .set("Client-ID", config.apis.twitch.client_id)
             .send({
+                "hub.callback": `http://webhook.typicalbot.com:${this.client.build === "stable" ? 5000 : this.client.build === "beta" ? 5001 : this.client.build === "development" ? 5002 : 5000}/webhook`,
+                "hub.topic": `https://api.twitch.tv/helix/streams?user_id=${id}`,
+                "hub.mode": "subscribe",
+                "hub.lease_seconds": 864000
+            })
+            .catch(err => { throw "Could not subscribe"; });
+    }
 
-            });
+    async fetchSubscriptions(guild) {
+        const subscriptions = await this.client.database.connection.table("webhooks").get(guild.id);
 
+        if (!subscriptions) await this.client.database.connection.insert({ "id": guild.id, "twitch": [] });
+
+        return subscriptions || { "id": guild.id, "twitch": [] };
+    }
+
+    async addSubscription(guild, id) {
+        const subscriptions = await this.client.database.connection.table("webhooks").get(id);
+        
+        if (!subscriptions) {
+            await this.subscribe(id).catch(err => { throw err; });
+            
+            return await this.client.database.connection.insert({ id, "guilds": [ guild.id ] });
+        }
+
+        if (subscriptions.guilds.includes(guild.id)) throw "Guild is already subscribed.";
+
+        const newList = subscriptions.guilds;
+        newList.push(guild.id);
+        
+        return await this.client.database.connection.table("webhooks").get(id).update({ guilds: newList });
+    }
+
+    async deleteSubscription(guild, id) {
+        const subscriptions = await this.client.database.connection.table("webhooks").get(id);
+        
+        if (!subscriptions || !subscriptions.guilds.includes(guild.id)) throw "Guild is not subscribed.";
+
+        const newList = subscriptions.guilds;
+        newList.splice(newList.indexOf(guild.id), 1);
+
+        if (!newList.length) {
+            await this.unsubscribe(id).catch(err => { throw err; });
+
+            return await this.client.database.connection.delete(id);
+        }
+        
+        return await this.client.database.connection.table("webhooks").get(id).update({ guilds: newList });
     }
 }
 
