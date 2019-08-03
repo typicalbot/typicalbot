d17001b4584aa22b497c93d0da4de73bd1d2309c
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2019-08-03 00:27:38 -0400

    Update Cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
     apps: [{
-        name: 'TypicalBot-PTB-Cluster-1',
+        name: 'Primula-PTB-1',
         script: 'cluster/index.js',
 
         autorestart: false,
         watch: false,
         env: {
-            CLUSTER: "ptb.typicalbot.cluster.1",
+            CLUSTER: "Primula PTB 1",
             SHARDS: '1-2'
         }
     },
     {
-        name: 'TypicalBot-PTB-Cluster-2',
+        name: 'Primula-PTB-2',
         script: 'cluster/index.js',
 
         autorestart: false,
         watch: false,
         env: {
-            CLUSTER: "ptb.typicalbot.cluster.2",
+            CLUSTER: "Primula PTB 2",
             SHARDS: '3-4'
         }
     }]
