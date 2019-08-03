d2e0936610c199f639d796b1ca4755315a96f257
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2019-08-03 17:38:11 -0400

    Update Cluster Generator

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,32 +1,29 @@
+const pm2 = require("pm2");
 const { Util } = require("discord.js");
 const config = require("./config");
 
-function generateClusters() {
-    return new Promise((resolve, reject) => {
-        Util.fetchRecommendedShards(config.token).then(shardCount => {
-            const shards = Array.from({ length: shardCount }, (a, b) => b);
-            const clusterCount = Math.ceil(shardCount / config.shardsPerCluster);
-            const clusters = new Array();
+async function generateClusters() {
+    const shardCount = await Util.fetchRecommendedShards(config.token);
+    const shards = Array.from({ length: shardCount }, (a, b) => b);
+    const clusterCount = Math.ceil(shardCount / config.shardsPerCluster);
+    const clusters = new Array();
 
-            for (let i = 1; i <= clusterCount; i++) {
-                const clusterShards = shards.splice(0, config.shardsPerCluster);
+    for (let i = 1; i <= clusterCount; i++) {
+        const clusterShards = shards.splice(0, config.shardsPerCluster);
 
-                clusters.push({
-                    name: `${config.clusterServer}-${config.clusterBuild ? `${config.clusterBuild}-` : ""}${i}`,
-                    script: './index.js',
-                    autorestart: false,
-                    watch: false,
-                    env: {
-                        CLUSTER: `${config.clusterServer} ${config.clusterBuild ? `${config.clusterBuild} ` : ""}${i}`,
-                        //SHARDS: `${clusterShards[0]}-${clusterShards[9] || clusterShards[clusterShards.length]}`
-                        SHARDS: clusterShards
-                    }
-                })
+        clusters.push({
+            name: `${config.clusterServer}-${config.clusterBuild ? `${config.clusterBuild}-` : ""}${i}`,
+            script: './index.js',
+            autorestart: false,
+            watch: false,
+            env: {
+                CLUSTER: `${config.clusterServer} ${config.clusterBuild ? `${config.clusterBuild} ` : ""}${i}`,
+                //SHARDS: `${clusterShards[0]}-${clusterShards[9] || clusterShards[clusterShards.length]}`
+                SHARDS: clusterShards
             }
+        })
+    }
 
-            resolve(clusters);
-        });
-    })
+    return clusters;
 }
 
-module.exports = generateClusters();
