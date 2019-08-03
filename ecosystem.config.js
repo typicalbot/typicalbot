b081442ddb2cb4f46d9436517f240b65dc1afc2d
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2019-08-03 14:11:42 -0400

    Update Cluster Generation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,27 +1,32 @@
 const { Util } = require("discord.js");
 const config = require("./config");
 
-const clusters = new Array();
+function generateClusters() {
+    return new Promise((resolve, reject) => {
+        Util.fetchRecommendedShards(config.token).then(shardCount => {
+            const shards = Array.from({ length: shardCount }, (a, b) => b);
+            const clusterCount = Math.ceil(shardCount / config.shardsPerCluster);
+            const clusters = new Array();
 
-Util.fetchRecommendedShards(config.token).then(shardCount => {
-    const clusterCount = Math.ceil(shardCount / config.shardsPerCluster);
-    const shards = Array.from({ length: shardCount }, (a, b) => b);
+            for (let i = 1; i <= clusterCount; i++) {
+                const clusterShards = shards.splice(0, config.shardsPerCluster);
 
-    for (let i = 1; i <= clusterCount; i++) {
-        const clusterShards = shards.splice(0, config.shardsPerCluster);
-
-        clusters.push({
-            name: `${config.clusterServer}-${config.clusterBuild ? `${config.clusterBuild}-` : ""}${i}`,
-            script: './index.js',
-            autorestart: false,
-            watch: false,
-            env: {
-                CLUSTER: `${config.clusterServer} ${config.clusterBuild ? `${config.clusterBuild} ` : ""}${i}`,
-                //SHARDS: `${clusterShards[0]}-${clusterShards[9] || clusterShards[clusterShards.length]}`
-                SHARDS: clusterShards
+                clusters.push({
+                    name: `${config.clusterServer}-${config.clusterBuild ? `${config.clusterBuild}-` : ""}${i}`,
+                    script: './index.js',
+                    autorestart: false,
+                    watch: false,
+                    env: {
+                        CLUSTER: `${config.clusterServer} ${config.clusterBuild ? `${config.clusterBuild} ` : ""}${i}`,
+                        //SHARDS: `${clusterShards[0]}-${clusterShards[9] || clusterShards[clusterShards.length]}`
+                        SHARDS: clusterShards
+                    }
+                })
             }
-        })
-    }
-});
 
-module.exports = clusters;
+            resolve(clusters);
+        });
+    })
+}
+
+module.exports = generateClusters();
