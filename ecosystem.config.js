2bf93c5afda5c1939293ab0ddf3338e7b58d584d
Bryan Pikaard <bryan.pikaardjr@outlook.com>
2019-08-03 17:42:27 -0400

    Update Cluster Generation

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,3 +27,19 @@ async function generateClusters() {
     return clusters;
 }
 
+pm2.connect(err => {
+    if (err) {
+        console.error(err);
+        process.exit(2);
+    }
+
+    pm2.start({
+        script: 'app.js',         // Script to be run
+        exec_mode: 'cluster',        // Allows your app to be clustered
+        instances: 4,                // Optional: Scales your app by 4
+        max_memory_restart: '100M'   // Optional: Restarts your app if it reaches 100Mo
+    }, function (err, apps) {
+        pm2.disconnect();   // Disconnects from PM2
+        if (err) throw err
+    });
+});
