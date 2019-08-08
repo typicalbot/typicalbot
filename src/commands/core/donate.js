3b7c8f283f1822007359c22106062e359194486b
Tyler Richards <rocket0191@gmail.com>
2019-08-07 19:20:56 -0500

    Update donate command

--- src/commands/core/donate.js
+++ src/commands/core/donate.js
@@ -4,7 +4,7 @@ const Constants = require(`../../utility/Constants`);
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            description: "Donate to the cause of TypicalBot.",
+            description: "Donate to support the development of TypicalBot.",
             usage: "donate",
             dm: true,
             mode: Constants.Modes.STRICT
@@ -12,14 +12,14 @@ module.exports = class extends Command {
     }
 
     execute(message, parameters, permissionLevel) {
-        message.send(`You can donate to my creator at **<${Constants.Links.DONATE}>** to help support my growth, development, and stability.`);
+        message.send(`You can donate to support the continued development of TypicalBot at **<${Constants.Links.DONATE}>** to help support my growth, development, and stability.`);
     }
 
     embedExecute(message, response) {
         message.buildEmbed()
             .setColor(0x00ADFF)
-            .setTitle("Donate to TypicalBot's Creator")
-            .setDescription(`You can donate to my creator at **<${Constants.Links.DONATE}>** to help support my growth, development, and stability.`)
+            .setTitle("Support the TypicalBot Project")
+            .setDescription(`You can donate support the continued development of TypicalBot at **<${Constants.Links.DONATE}>** to help support my growth, development, and stability.`)
             .setFooter("TypicalBot", Constants.Links.ICON)
             .setTimestamp()
             .send();
