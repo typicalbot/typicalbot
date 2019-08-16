ba577137423e399bb5945d2853b15f123a8e269d
nsylke <hello@nsylke.me>
2019-08-16 11:08:41 -0500

    Add calculator command

---
+++ src/commands/utility/calculator.js
@@ -0,0 +1,35 @@
+const Command = require("../../structures/Command");
+const Constants = require("../../utility/Constants");
+const math = require("mathjs");
+
+module.exports = class extends Command {
+    constructor(...args) {
+        super(...args, {
+            description: "Evaluate math expressions.",
+            usage: "math <expression>",
+            aliases: ["calc", "math"],
+            mode: Constants.Modes.LITE
+        });
+    }
+
+    execute(message, parameters, permissionLevel) {
+        if (!parameters) return message.error(this.client.functions.error("usage", this));
+
+        try {
+            const result = math.eval(parameters);
+
+            message.send(
+                "**INPUT:**"
+                + `\`\`\`\n`
+                + `${parameters}`
+                + `\`\`\`\n`
+                + "**OUTPUT:**"
+                + `\`\`\`\n`
+                + `${result}`
+                + `\`\`\``
+            );
+        } catch (e) {
+            message.error("Failed to evaluate math expression.");
+        }
+    }
+};
