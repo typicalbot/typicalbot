7d62ce0bdbab60326e3ffd4b500ea894485275f2
Tyler Richards <rocket0191@gmail.com>
2019-08-20 14:06:46 -0500

    Improve randomuser command
    Allows filtering out offline/invisible members

--- src/commands/utility/randomuser.js
+++ src/commands/utility/randomuser.js
@@ -4,14 +4,15 @@ const Constants = require(`../../utility/Constants`);
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            description: "Selects a random member in the server..",
-            usage: "randomuser",
+            description: "Selects a random member in the server.",
+            usage: "randomuser [online]",
             mode: Constants.Modes.LITE
         });
     }
 
     execute(message, parameters, permissionLevel) {
-        const user = message.guild.members.random().user;
+        const members = parameters === "online" ? message.guild.members.filter(m => m.presence.status === "online") : message.guild.members;
+        const user = members.random().user;
 
         message.send(`Your random pick is: **${user.username}#${user.discriminator}** (${user.id}).`);
     }
