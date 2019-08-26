94e6fca3ee751001e3478502cb193e30d54192a3
Tyler Richards <rocket0191@gmail.com>
2019-08-25 19:30:42 -0500

    Fix bug in guildCreate event

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -8,7 +8,7 @@ class GuildCreate extends Event {
     async execute(guild) {
         if (!guild.available) return;
 
-        if (this.client.build === "stable") this.client.sendStatistics(message.guild.shardID);
+        if (this.client.build === "stable") this.client.sendStatistics(guild.shardID);
     }
 }
 
