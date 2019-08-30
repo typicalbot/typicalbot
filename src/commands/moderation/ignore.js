8477984157eb011691287c8c207d65ad1c386a7c
Tyler Richards <11968358+tjrgg@users.noreply.github.com>
2019-08-30 12:36:19 -0500

    Fix description of ignore command

--- src/commands/moderation/ignore.js
+++ src/commands/moderation/ignore.js
@@ -4,7 +4,7 @@ const Constants = require(`../../utility/Constants`);
 module.exports = class extends Command {
     constructor(...args) {
         super(...args, {
-            description: "Makes the bot ignore commands or invites in a channel.",
+            description: "Makes the bot ignore commands, invites and stars in a channel.",
             usage: "ignore ['view'|'commands'|'invites'|'stars']",
             permission: Constants.Permissions.Levels.SERVER_ADMINISTRATOR,
             mode: Constants.Modes.STRICT
