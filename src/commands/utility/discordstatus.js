ed0039a35fee3dadd96973d121f0b0f1ac8b7e23
nsylke <hello@nsylke.me>
2019-09-10 23:31:22 -0500

    Add discord status URL & incident URL

--- src/commands/utility/discordstatus.js
+++ src/commands/utility/discordstatus.js
@@ -16,11 +16,11 @@ module.exports = class extends Command {
         fetch('https://status.discordapp.com/api/v2/summary.json')
             .then(res => res.json())
             .then(json => {
-                if (json.incidents.length === 0) return message.send('All systems are operational.');
+                if (json.incidents.length === 0) return message.send('All systems are operational.\n\n<https://status.discordapp.com>');
 
-                const incident = json.incident;
+                const incident = json.incidents[0];
 
-                message.send(`**Incident** at **${new Date(incident.created_at)}**\n${incident.incident_updates[0].body}`);
+                message.send(`**Incident** at **${new Date(incident.created_at)}**\n${incident.incident_updates[0].body}\n\n<https://status.discordapp.com/incidents/${incident.incident_id}>`);
             })
             .catch(err => err ? null : message.error('An error occurred making that request.'));
     }
