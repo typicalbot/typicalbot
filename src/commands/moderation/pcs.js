5a392074b1265bf2b0b0234443518d23ef341dd2
HyperCoder <bryan.pikaardjr@outlook.com>
2019-09-12 20:23:55 -0400

    Update pcs command to add list and clear

--- src/commands/moderation/pcs.js
+++ src/commands/moderation/pcs.js
@@ -14,12 +14,16 @@ module.exports = class extends Command {
     }
 
     async execute(message, parameters) {
-        const args = /(list|clear|add|remove)(?:\s+(\w+)(?:\s+(.+))?)?/i.exec(parameters);
+        const args = /(list|clear|add|remove)(?:\s+(\w+)(?:\s+([\W\w]+))?)?/i.exec(parameters);
         if (!args) return message.error(this.client.functions.error("usage", this));
 
         const actualUserPermissions = await this.client.handlers.permissions.fetch(message.guild, message.author, true);
 
-        const action = args[1],
+        const action = args[1];
+        const command = args[2];
+        const optionsSet = args[3];
+
+        /*const action = args[1],
             command = args[2],
             sub = args[3],
             responseA = sub ? /\[\[response:\s*([A-Za-z0-9\s!@#$%^&*().,?:;\-_=+"\\'\/<>]+)\]\]/i.exec(sub) : null,
@@ -33,17 +37,53 @@ module.exports = class extends Command {
             addRoles = addRolesA ? addRolesA[1] : null,
             removeRoles = removeRolesA ? removeRolesA[1] : null,
             reqPermissions = reqPermissionsA ? reqPermissionsA[1] : null,
-            msgDelete = msgDeleteA ? msgDeleteA[1] : null;
+            msgDelete = msgDeleteA ? msgDeleteA[1] : null;*/
 
         if (action === "list") {
+            let page = isNaN(command) ? 1 : command;
+            const commands = message.guild.settings.pcs;
 
+            if (!commands.length) return message.send("There are no personal commands setup.");
+
+            const count = Math.ceil(commands.length / 10);
+            if (page < 1 || page > count) page = 1;
+
+            const list = commands.splice((page - 1) * 10, 10).map((k, i) => ` • **${i + 1}:** ${k.command}`);
+
+            message.send(`**__Personal Commands__**\n\n**Page ${page} / ${count}**\n${list.join("\n")}`);
         } else if (action === "clear") {
             if (actualUserPermissions.level < Constants.Permissions.Levels.SERVER_ADMINISTRATOR) return message.error(this.client.functions.error("perms", { permission: 3 }, actualUserPermissions));
 
+            this.client.settings.update(message.guild.id, { pcs: {} }).then(() => {
+                this.client.settings.update(message.guild.id, { pcs: [] }).then(() => {
+                    message.reply("Successfully cleared personal commands.");
+                }).catch(err => {
+                    throw err;
+                })
+            }).catch(err => {
+                message.reply("Failed to create personal command.");
+            });
         } else if (action === "add") {
+            if (!optionsSet) return message.error("You cannot add a custom command without options.");
             if (actualUserPermissions.level < Constants.Permissions.Levels.SERVER_ADMINISTRATOR) return message.error(this.client.functions.error("perms", { permission: 3 }, actualUserPermissions));
             if (!command) return message.error("You must provide a command.");
             if (command.length > 14) return message.error("You cannot add a custom command with a name over 14 characters long.");
+
+            const options = /(--?[\w]+)\s+([^-]+)/g.exec(optionsSet);
+
+            const responseA = sub ? /\[\[response:\s*([A-Za-z0-9\s!@#$%^&*().,?:;\-_=+"\\'\/<>]+)\]\]/i.exec(sub) : null,
+                dmA = sub ? /\[\[dm:\s*([A-Za-z0-9\s!@#$%^&*().,?:;\-_=+"\\'\/<>]+)\]\]/i.exec(sub) : null,
+                addRolesA = sub ? /\[\[\+role:\s*([A-Za-z0-9\s!@#$%^&*().,?:;\-_=+"\\'\/<>]+)\]\]/i.exec(sub) : null,
+                removeRolesA = sub ? /\[\[\-role:\s*([A-Za-z0-9\s!@#$%^&*().,?:;\-_=+"\\'\/<>]+)\]\]/i.exec(sub) : null,
+                reqPermissionsA = sub ? /(?:(?:-p|--permissions)\s+(\d+))/i.exec(sub) : null,
+                msgDeleteA = sub ? /(-d|--delete)/i.exec(sub) : null,
+                response = responseA ? responseA[1] : null,
+                dm = dmA ? dmA[1] : null,
+                addRoles = addRolesA ? addRolesA[1] : null,
+                removeRoles = removeRolesA ? removeRolesA[1] : null,
+                reqPermissions = reqPermissionsA ? reqPermissionsA[1] : null,
+                msgDelete = msgDeleteA ? msgDeleteA[1] : null,
+
             if (reqPermissions && reqPermissions < Constants.Permissions.Levels.SERVER_MEMBER) return message.error("You cannot use a permission level less than Server Member.");
             if (reqPermissions && reqPermissions > Constants.Permissions.Levels.SERVER_OWNER) return message.error("You cannot use a permission level greater than Server Owner.");
 
