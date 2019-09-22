2e08c1c60ba8736bb16914aec4e91428221fb3ec
Skillz <gankstarsleague@gmail.com>
2019-09-22 10:54:29 -0400

    convert bunny command to ts

---
+++ src/commands/fun/bunny.ts
@@ -0,0 +1,20 @@
+import { Message, TextChannel, MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import Command from '../../structures/Command';
+
+export default class extends Command {
+    aliases =['rabbit'];
+
+    async execute(message: Message) {
+        const type = Math.random() <= 0.25 ? 'gif' : 'poster';
+
+        const data = await fetch(`https://api.bunnies.io/v2/loop/random/?media=${type}`)
+            .then((res) => res.json())
+            .catch(() => message.error(this.client.translate('common:REQUEST_ERROR')));
+
+        const canSendEmbed = message.guild && message.guild.me && message.guild.settings.embed && (message.channel as TextChannel).permissionsFor(message.guild.me);
+        if (!canSendEmbed) return message.send(data.media[type]);
+
+        return message.send(new MessageEmbed().setColor(0x00adff).setImage(data.media[type]));
+    }
+}
