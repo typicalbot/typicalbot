3c961a6c132c7e5a13a726fd9409284f88ca1073
Skillz <gankstarsleague@gmail.com>
2019-09-22 10:54:13 -0400

    convert advice command to ts

---
+++ src/commands/fun/advice.ts
@@ -0,0 +1,12 @@
+import { Message } from 'discord.js';
+import fetch from 'node-fetch';
+import Command from '../../structures/Command';
+
+export default class extends Command {
+    execute(message: Message) {
+        fetch('https://api.adviceslip.com/advice')
+            .then((res) => res.json())
+            .then((json) => message.send(json.slip.advice))
+            .catch(() => message.error(this.client.translate('common:REQUEST_ERROR')));
+    }
+}
