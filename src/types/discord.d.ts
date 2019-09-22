041d529771cf0b5e03cde8700d15ee60f609e65b
Skillz <gankstarsleague@gmail.com>
2019-09-22 00:29:49 -0400

    moves types into lib folder

--- src/types/discord.d.ts
+++
@@ -1,7 +0,0 @@
-import { MessageEmbed } from 'discord.js';
-
-declare module 'discord.js' {
-  interface Message {
-    error(content: string, embed?: MessageEmbed, options?: MessageOptions): Promise<Message>;
-  }
-}
--- src/types/typicalbot.d.ts
+++
@@ -1,9 +0,0 @@
-export interface CommandOptions {
-  description?: string;
-  usage?: string;
-  aliases?: string[];
-  dm?: boolean;
-  permission?: -1 | 0 | 1 | 2 | 3 | 4 | 10;
-  mode?: 0 | 1 | 2;
-  access?: 0 | 1 | 3;
-}
