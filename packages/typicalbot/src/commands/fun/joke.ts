942ca479f2db178aeed50154058fa266b33d2941
Skillz <gankstarsleague@gmail.com>
2019-09-25 21:40:18 -0400

    joke to i18n and ts

---
+++ packages/typicalbot/src/commands/fun/joke.ts
@@ -0,0 +1,10 @@
+import { Message } from 'discord.js'
+import Command from '../../structures/Command';
+import * as jokes from '../../utility/jokes.json';
+
+export default class extends Command {
+
+    static execute(message: Message) {
+        return message.send(jokes[Math.floor(Math.random() * jokes.length)]);
+    }
+};
