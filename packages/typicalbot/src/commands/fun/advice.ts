5dc5d0ea0e25b81ff7cefbefb3d0b005e8fbd169
Skillz <gankstarsleague@gmail.com>
2019-09-25 21:35:54 -0400

    fix advice static

--- packages/typicalbot/src/commands/fun/advice.ts
+++ packages/typicalbot/src/commands/fun/advice.ts
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 import Command from '../../structures/Command';
 
 export default class extends Command {
-    execute(message: Message) {
+    static execute(message: Message) {
         fetch('https://api.adviceslip.com/advice')
             .then((res) => res.json())
             .then((json) => message.send(json.slip.advice))
