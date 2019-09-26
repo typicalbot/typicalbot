5f9e8cb2132b595edfca43a7194ee3f291324e0b
Skillz <gankstarsleague@gmail.com>
2019-09-25 21:37:04 -0400

    dice to ts and i18n

---
+++ packages/typicalbot/src/commands/fun/dice.ts
@@ -0,0 +1,14 @@
+import { Message } from 'discord.js';
+import Command from '../../structures/Command'
+
+export default class extends Command {
+    aliases = ['die']
+
+    static execute(message: Message, parameters = "6") {
+        const sides = +parameters || 6
+
+        if (sides < 2 || sides > 100 || sides % 1 !== 0) return message.error("dice:INVALID");
+
+        return message.reply(message.translate("dice:RESPONSE", { amount: Math.floor(Math.random() * sides) + 1 }));
+    }
+};
