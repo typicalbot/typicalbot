cf11669fc020e6360914b4cd6b81bfc828cde25b
Skillz <gankstarsleague@gmail.com>
2019-09-25 21:40:08 -0400

    hug to i18n and ts

---
+++ packages/typicalbot/src/commands/fun/hug.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js'
+import Command from '../../structures/Command'
+
+export default class extends Command {
+
+    static execute(message: Message) {
+        const mention = message.mentions.users.first();
+
+        const randomAddon = Math.random() <= 0.25;
+
+        // TODO: fix this if discord.js fixes partials behavior
+        if (!mention || mention.id === (message.author && message.author.id)) return message.reply(message.translate('hug:SELF'));
+
+        return message.reply(message.translate(randomAddon ? 'hug:RESPONSE_AWW' : 'hug:RESPONSE', { user: mention }));
+    }
+};
