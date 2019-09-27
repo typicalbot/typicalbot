33cf2df5deabfec33119fde124dcf8bd4a58862d
Skillz <gankstarsleague@gmail.com>
2019-09-26 20:43:58 -0400

    ping command to ts and i18n

---
+++ packages/typicalbot/src/commands/core/ping.ts
@@ -0,0 +1,15 @@
+import { Message } from 'discord.js';
+import Command from '../../structures/Command';
+import Constants from '../../utility/Constants';
+
+export default class extends Command {
+    dm = true;
+
+    mode = Constants.Modes.STRICT;
+
+    async execute(message: Message) {
+        const ping = await message.send('ping:CALCULATING');
+
+        return ping.edit(message.translate('ping:RESPONSE', { command: ping.createdTimestamp - message.createdTimestamp, api: Math.floor(this.client.ws.ping) }));
+    }
+};
