e8bf985858d03789d49dfda53316402e388aa202
Skillz <gankstarsleague@gmail.com>
2019-09-26 20:43:45 -0400

    invite command to ts and i18n

---
+++ packages/typicalbot/src/commands/core/invite.ts
@@ -0,0 +1,20 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Command from '../../structures/Command';
+import Constants from '../../utility/Constants';
+
+export default class extends Command {
+    dm = true;
+
+    mode = Constants.Modes.STRICT;
+
+    static execute(message: Message) {
+        if (!message.embedable) return message.reply(message.translate('invite:TEXT', { link: Constants.Links.OAUTH }));
+
+        return message.reply(new MessageEmbed()
+            .setColor(0x00adff)
+            .setTitle(message.translate('invite:TYPICAL_INVITE'))
+            .setDescription(message.translate('invite:TEXT', { link: Constants.Links.OAUTH }))
+            .setFooter('TypicalBot', Constants.Links.ICON)
+            .setTimestamp())
+    }
+};
