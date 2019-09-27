4668b2c495612d2c768a300c9ab857869f8d2f9e
Skillz <gankstarsleague@gmail.com>
2019-09-26 20:43:17 -0400

    credits command to ts and i18n

---
+++ packages/typicalbot/src/commands/core/credits.ts
@@ -0,0 +1,23 @@
+import { Message, MessageEmbed } from 'discord.js';
+import Command from '../../structures/Command';
+import Constants from '../../utility/Constants';
+
+export default class extends Command {
+    dm = true;
+
+    mode = Constants.Modes.STRICT;
+
+    static execute(message: Message) {
+        if (!message.embedable) return message.send(message.translate('credits:TEXT'));
+
+        return message.send(new MessageEmbed()
+            .setColor(0x00ADFF)
+            .setTitle(message.translate('credits:CREDITS'))
+            .setDescription(message.translate('credits:TEXT'))
+            .addField('» Aklixio#0588 (84430408447426560)', message.translate('credits:DESIGNER'))
+            .setFooter('TypicalBot', Constants.Links.ICON)
+            .setTimestamp()
+        )
+    }
+
+};
