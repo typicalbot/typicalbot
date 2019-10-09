de9906881c2cff3d98972df6c5a729381eb9a349
Skillz4Killz <admin@ezlgg.com>
2019-10-09 10:09:30 -0400

    bots command to ts and i18n

---
+++ packages/typicalbot/src/commands/utility/bots.ts
@@ -0,0 +1,47 @@
+import fetch from 'node-fetch';
+import Command from '../../structures/Command';
+import Constants from '../../utility/Constants';
+import { TypicalGuildMessage } from '../../types/typicalbot';
+import { CarboniteBots } from '../../types/apis';
+
+const regex = /(\d+)?/i;
+const replaceFirstRegex = /[^a-z0-9]/gmi;
+const replaceSecondRegex = /\s+/g;
+
+export default class extends Command {
+    mode = Constants.Modes.LITE;
+
+    async execute(message: TypicalGuildMessage, parameters: string) {
+        const args = regex.exec(parameters) || [];
+        args.shift();
+        const [number] = args;
+
+        const data = await fetch('https://www.carbonitex.net/discord/api/listedbots')
+            .then((res) => res.json())
+            .catch(() => null) as CarboniteBots[] | null;
+        if (!data) return message.error('common:REQUEST_ERROR');
+
+        const page = number ? parseInt(number, 10) : 1;
+
+        const bots = data
+            .filter((bot) => parseInt(bot.botid, 10) > 10 && parseInt(bot.servercount, 10) > 0)
+            .sort((a, b) => parseInt(b.servercount, 10) - parseInt(a.servercount, 10))
+            .map(bot => ({
+                ...bot,
+                name: this.name.replace(replaceFirstRegex, '').replace(replaceSecondRegex, ''),
+                servercount: Number(bot.servercount).toLocaleString()
+            }));
+
+        const content = this.client.helpers.pagify.execute(
+            bots.map((bot) => `${this.client.helpers.lengthen.execute(bot.name, 20)}: ${bot.servercount}${bot.compliant ? message.translate('bots:COMPLIANT') : ''}`),
+            page,
+        );
+
+        return message.send([
+            message.translate('bots:RANKED'),
+            '```autohotkey',
+            content,
+            '```'
+        ].join('\n'));
+    }
+};
