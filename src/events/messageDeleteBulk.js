e09468ad9de642500794e58adfbeb9387bfea268
Tyler Richards <11968358+tjrgg@users.noreply.github.com>
2019-10-13 19:23:08 -0500

    fix: Update `messageDeleteBulk` event to remove haste

--- src/events/messageDeleteBulk.js
+++ src/events/messageDeleteBulk.js
@@ -2,25 +2,6 @@ const Event = require("../structures/Event");
 const moment = require("moment");
 const fetch = require("node-fetch");
 
-async function hastebin(input, options = {}) {
-    if (typeof options === "string") options = { url: "https://www.hastebin.com", extension: options };
-
-    const url = "url" in options ? options.url : "https://www.hastebin.com";
-    const extension = "extension" in options ? options.extension : "js";
-
-    const res = await fetch(`${url}/documents`, {
-        method: "post",
-        body: input,
-        headers: { "Content-Type": "text/plain" }
-    });
-
-    if (!res.ok) console.error(res.statusText);
-
-    const { key } = await res.json();
-
-    return `${url}/${key}.${extension}`;
-}
-
 class MessageBulkDelete extends Event {
     constructor(...args) {
         super(...args);
@@ -40,18 +21,16 @@ class MessageBulkDelete extends Event {
         if (!logsChannel) return;
 
         const channel = message.channel;
-
-        const haste = await hastebin(messages.map(m => `${moment(m.createdAt).format("dddd MMMM Do, YYYY, hh:mm A")} | ${m.author.tag} (${m.author.id}):\n${m.content}`).join("\n\n--  --  --  --  --\n\n"));
-
+        
         if (settings.logs.delete === "--embed") return logsChannel.buildEmbed()
             .setColor(0x3EA7ED)
-            .setDescription(`${messages.size} messages were purged: ${haste}`)
+            .setDescription(`${messages.size} messages were purged.`)
             .setFooter(`Messages Purged in #${channel.name} (${channel.id})`)
             .setTimestamp()
             .send()
             .catch(() => { return; });
 
-        logsChannel.send(`${messages.size} messages were purged in ${channel.toString()} (${channel.id}): ${haste}`).catch(() => { return; });
+        logsChannel.send(`${messages.size} messages were purged in ${channel.toString()} (${channel.id}).`).catch(() => { return; });
     }
 }
 
