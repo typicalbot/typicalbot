bb13f7546d755d2b72f55c9e7372a54422133aa6
nsylke <hello@nsylke.me>
2020-03-26 19:01:41 -0500

    feat: rename joke command to pun; use icanhazdadjoke api

--- src/commands/fun/joke.ts
+++
@@ -1,9 +0,0 @@
-import Command from '../../structures/Command';
-import { TypicalGuildMessage } from '../../types/typicalbot';
-import jokes from '../../utility/jokes.json';
-
-export default class extends Command {
-    execute(message: TypicalGuildMessage) {
-        return message.send(jokes[Math.floor(Math.random() * jokes.length)]);
-    }
-}
---
+++ src/commands/fun/pun.ts
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch';
+import Command from '../../structures/Command';
+import { TypicalGuildMessage } from '../../types/typicalbot';
+
+export default class extends Command {
+    aliases = ['dadjoke'];
+
+    execute(message: TypicalGuildMessage) {
+        fetch('https://icanhazdadjoke.com', { headers: { 'Accept': 'application/json' } })
+            .then((res) => res.json())
+            .then((json) => message.send(json.joke))
+            .catch(() => message.error(message.translate('common:REQUEST_ERROR')));
+    }
+}
