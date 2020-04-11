bf347552423e1e8230a209bbdaedecbfc115bf9a
nsylke <hello@nsylke.me>
2020-04-11 17:57:52 -0500

    feat: bird command using some-random-api

---
+++ src/commands/fun/bird.ts
@@ -0,0 +1,19 @@
+import { MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import Command from '../../structures/Command';
+import { TypicalGuildMessage } from '../../types/typicalbot';
+
+export default class extends Command {
+    aliases = ['birb']
+
+    async execute(message: TypicalGuildMessage) {
+        const data = await fetch('https://some-random-api.ml/img/birb')
+            .then((res) => res.json())
+            .catch(() =>
+                message.error(message.translate('common:REQUEST_ERROR')));
+
+        if (!message.embeddable) return message.send(data.link);
+
+        return message.send(new MessageEmbed().setColor(0x00adff).setImage(data.link));
+    }
+}
