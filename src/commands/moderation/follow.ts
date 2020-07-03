9582696609d3f03f721b3e1d8c25e2ad4b1f3075
Skillz <gankstarsleague@gmail.com>
2020-07-03 11:03:52 -0400

    follow work

---
+++ src/commands/moderation/follow.ts
@@ -0,0 +1,33 @@
+import Command from '../../lib/structures/Command';
+import { TypicalGuildMessage } from '../../lib/types/typicalbot';
+import { Modes, PermissionsLevels } from '../../lib/utils/constants';
+
+export default class extends Command {
+    mode = Modes.STRICT;
+
+    async execute(message: TypicalGuildMessage, parameters: string) {
+        const [type, id] = parameters;
+
+        const permissions = await message.member.fetchPermissions(true);
+
+        if (permissions.level < PermissionsLevels.SERVER_ADMINISTRATOR) {
+            // eslint-disable-next-line max-len
+            return message.error(
+                this.client.helpers.permissionError.execute(
+                    message,
+                    this,
+                    permissions,
+                    PermissionsLevels.SERVER_ADMINISTRATOR
+                )
+            );
+        }
+
+        const channel = message.mentions.channels.first() ?? message.guild.channels.cache.get(id) ?? message.channel;
+        const isStatus = type?.toLowerCase() === 'status';
+
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        this.client.api.channels(isStatus ? '621817852726607882' : '268559149175013376').followers.post({ data: { webhook_channel_id: channel.id } });
+        return message.success(message.translate(isStatus ? 'moderation/follow:FOLLOWED_STATUS' : 'moderation/follow:FOLLOWED'));
+    }
+}
