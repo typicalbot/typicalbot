54efa20290cbc5afe35e7cc2d12d11640d884aac
Nicholas Sylke <nicksylke@outlook.com>
2020-07-03 08:03:24 -0500

    feat: fox command (animal)

---
+++ src/commands/animals/fox.ts
@@ -0,0 +1,17 @@
+import { MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import Command from '../../lib/structures/Command';
+import { TypicalGuildMessage } from '../../lib/types/typicalbot';
+
+export default class extends Command {
+    async execute(message: TypicalGuildMessage) {
+        const data = await fetch('https://some-random-api.ml/img/fox')
+            .then((res) => res.json())
+            .catch(() =>
+                message.error(message.translate('common:REQUEST_ERROR')));
+
+        if (!message.embeddable) return message.send(data.link);
+
+        return message.send(new MessageEmbed().setColor(0x00adff).setImage(data.link));
+    }
+}
