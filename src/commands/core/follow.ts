477e6366d2a8b6b2dda21bf267d972d2d9ee8ac6
Skillz <gankstarsleague@gmail.com>
2020-07-07 09:37:55 -0400

    fix follow bug

--- src/commands/core/follow.ts
+++ src/commands/core/follow.ts
@@ -4,27 +4,14 @@ import { Modes, PermissionsLevels } from '../../lib/utils/constants';
 
 export default class extends Command {
     mode = Modes.STRICT;
+    permission = PermissionsLevels.SERVER_ADMINISTRATOR;
 
     async execute(message: TypicalGuildMessage, parameters: string) {
-        const [type, id] = parameters;
-
-        const permissions = await message.member.fetchPermissions(true);
-
-        if (permissions.level < PermissionsLevels.SERVER_ADMINISTRATOR) {
-            // eslint-disable-next-line max-len
-            return message.error(
-                this.client.helpers.permissionError.execute(
-                    message,
-                    this,
-                    permissions,
-                    PermissionsLevels.SERVER_ADMINISTRATOR
-                )
-            );
-        }
-
+        const [type, id] = parameters.split(' ');
         const channel = message.mentions.channels.first() ?? message.guild.channels.cache.get(id) ?? message.channel;
         const isStatus = type?.toLowerCase() === 'status';
 
+        console.log('test this', parameters, type, isStatus);
         // @ts-ignore
         // eslint-disable-next-line @typescript-eslint/camelcase
         this.client.api.channels(isStatus ? '621817852726607882' : '268559149175013376').followers.post({ data: { webhook_channel_id: channel.id } });
