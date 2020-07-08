ed94da3b455f59848d2d665f9455658815f6f782
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-07-07 21:49:50 -0400

    feat: `follow` command to follow TB news channels (#281)
    * follow work
    
    * mod => core
    
    * fix follow bug
    
    * remove logs
    
    * perm handling
    
    * Update src/commands/core/follow.ts
    
    Co-authored-by: Nicholas Sylke <nicksylke@outlook.com>
    
    * cleanup
    
    * remove useless line
    
    * remove unused string
    
    * add true back
    
    Co-authored-by: Nicholas Sylke <nicksylke@outlook.com>

---
+++ src/commands/core/follow.ts
@@ -0,0 +1,24 @@
+import Command from '../../lib/structures/Command';
+import { TypicalGuildMessage } from '../../lib/types/typicalbot';
+import { Modes, PermissionsLevels } from '../../lib/utils/constants';
+
+export default class extends Command {
+    mode = Modes.STRICT;
+    permission = PermissionsLevels.SERVER_ADMINISTRATOR;
+
+    async execute(message: TypicalGuildMessage, parameters: string) {
+        const [type, id] = parameters.split(' ');
+        const channel = message.mentions.channels.first() ?? message.guild.channels.cache.get(id) ?? message.channel;
+        if (!message.guild.me?.permissions.has('MANAGE_WEBHOOKS', true))
+            return message.error(message.translate('common:INSUFFICIENT_PERMISSIONS', {
+                permission: 'Manage Webhooks'
+            }));
+
+        const isStatus = type?.toLowerCase() === 'status';
+
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        this.client.api.channels(isStatus ? '621817852726607882' : '268559149175013376').followers.post({ data: { webhook_channel_id: channel.id } });
+        return message.success(message.translate(isStatus ? 'core/follow:FOLLOWED_STATUS' : 'core/follow:FOLLOWED'));
+    }
+}
