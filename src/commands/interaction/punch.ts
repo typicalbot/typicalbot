17eb61b78186e8ee90c13f236dcb410316caf566
Nicholas Sylke <nicksylke@outlook.com>
2020-07-26 11:56:27 -0500

    refactor: move cookie, hug, punch, shoot, slap, stab into new category

--- src/commands/interaction/punch.ts
+++ src/commands/interaction/punch.ts
@@ -4,12 +4,14 @@ import { TypicalGuildMessage } from '../../lib/types/typicalbot';
 export default class extends Command {
     execute(message: TypicalGuildMessage) {
         const mention = message.mentions.users.first();
+
         const randomAddon = Math.random() <= 0.25;
 
         if (!mention || mention.id === message.author.id)
-            return message.reply(message.translate(randomAddon
-                ? 'fun/cookie:SELF_LAUGH'
-                : 'fun/cookie:SELF_KEPT'));
-        return message.reply(message.translate('fun/cookie:GIVEN', { user: mention.toString() }));
+            return message.reply(message.translate('interaction/punch:SELF'));
+
+        return message.reply(message.translate(randomAddon ? 'interaction/punch:RESPONSE_JAW' : 'interaction/punch:RESPONSE', {
+            user: mention.toString()
+        }));
     }
 }
--- src/commands/interaction/slap.ts
+++ src/commands/interaction/slap.ts
@@ -4,13 +4,12 @@ import { TypicalGuildMessage } from '../../lib/types/typicalbot';
 export default class extends Command {
     execute(message: TypicalGuildMessage) {
         const mention = message.mentions.users.first();
-
         const randomAddon = Math.random() <= 0.25;
 
         if (!mention || mention.id === message.author.id)
-            return message.reply(message.translate('fun/hug:SELF'));
+            return message.reply(message.translate('interaction/slap:SELF'));
 
-        return message.reply(message.translate(randomAddon ? 'fun/hug:RESPONSE_AWW' : 'fun/hug:RESPONSE', {
+        return message.reply(message.translate(randomAddon ? 'interaction/slap:RESPONSE_HURT' : 'interaction/slap:RESPONSE', {
             user: mention.toString()
         }));
     }
