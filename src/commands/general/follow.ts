094833ebec06b8d2223aecdf7f3b446606355fde
Nicholas Sylke <nicksylke@outlook.com>
2020-08-01 21:13:28 -0500

    refactor: follow to use message method for following

--- src/commands/general/follow.ts
+++ src/commands/general/follow.ts
@@ -7,8 +7,7 @@ export default class extends Command {
     permission = PermissionsLevels.SERVER_ADMINISTRATOR;
 
     async execute(message: TypicalGuildMessage, parameters: string) {
-        const [type, id] = parameters.split(' ');
-        const channel = message.mentions.channels.first() ?? message.guild.channels.cache.get(id) ?? message.channel;
+        const [type] = parameters;
         if (!message.guild.me?.permissions.has('MANAGE_WEBHOOKS', true))
             return message.error(message.translate('common:INSUFFICIENT_PERMISSIONS', {
                 permission: 'Manage Webhooks'
@@ -19,9 +18,7 @@ export default class extends Command {
 
         const isStatus = type?.toLowerCase() === 'status';
 
-        // @ts-ignore
-        // eslint-disable-next-line @typescript-eslint/camelcase
-        this.client.api.channels(isStatus ? '621817852726607882' : '268559149175013376').followers.post({ data: { webhook_channel_id: channel.id } });
+        await message.followers(isStatus ? '621817852726607882' : '268559149175013376');
         return message.success(message.translate(isStatus ? 'general/follow:FOLLOWED_STATUS' : 'general/follow:FOLLOWED'));
     }
 }
