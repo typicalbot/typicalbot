22fdada72675f8ee7d93c437dda3a5a6fd609625
Nicholas Sylke <nicksylke@outlook.com>
2020-08-22 07:49:20 -0500

    refactor: cleanup ipc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import dotenv from 'dotenv';
 import fs from 'fs';
-import { TextChannel } from 'discord.js';
 import { Client, ClientSocket, NodeMessage } from 'veza';
 import Cluster from './lib/TypicalClient';
 
@@ -35,46 +34,6 @@ if (!process.env.CLUSTERED) {
             message.reply(eval(`client.${message.data.data}`));
         } else if (message.data.event === 'shardCount') {
             message.reply(client.shardCount);
-        } else if (message.data.event === 'channelEmbed') {
-            const { apiKey, channel, json } = message.data;
-
-            const guild = Buffer.from(apiKey.split('.')[0], 'base64').toString('utf-8');
-
-            const trueGuild = client.guilds.cache.get(guild);
-            if (!trueGuild)
-                return message.reply({ response: "Guild doesn't exist." });
-
-            const settings = await client.settings.fetch(guild);
-            const trueApiKey = settings.apikey;
-
-            if (apiKey !== trueApiKey)
-                return message.reply({ response: 'Invalid API key.' });
-
-            if (!trueGuild.channels.cache.has(channel))
-                return message.reply({ response: "Channel doesn't exist." });
-
-            const trueChannel = trueGuild.channels.cache.get(channel);
-            if (trueChannel instanceof TextChannel) {
-                const botPerms = trueChannel.permissionsFor(process.env.ID!);
-                if (
-                    !botPerms ||
-                    !botPerms.has(['VIEW_CHANNEL', 'SEND_MESSAGES'])
-                )
-                    return message.reply({
-                        response:
-                            'Missing permissions to read or send on this channel.'
-                    });
-
-                return trueChannel
-                    .send('', json)
-                    .then(() => message.reply({ response: 'Success' }))
-                    .catch(() =>
-                        message.reply({ response: 'An error occurred.' }));
-            }
-
-            return message.reply({
-                response: 'Invalid channel type provided.'
-            });
         }
     });
 }
