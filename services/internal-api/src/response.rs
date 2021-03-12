15990250e1556c086fffc2425107a3a7761da08c
Nicholas Sylke <nicksylke@outlook.com>
2021-03-12 11:14:58 -0600

    feat: internal api w/ pgsql

---
+++ services/internal-api/src/response.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Response<T> {
+    pub results: Vec<T>
+}
+
+impl<T> Response<T> {
+    pub fn new() -> Self {
+        Self { results: vec![] }
+    }
+}
---
+++ services/internal-api/src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    announcements (id) {
+        id -> Uuid,
+        created_at -> Timestamp,
+        message -> Text
+    }
+}
