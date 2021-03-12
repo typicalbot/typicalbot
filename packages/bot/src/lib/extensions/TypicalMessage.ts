9c7145186629b440ea0f50abfae78331e5460930
Nicholas Sylke <nicksylke@outlook.com>
2021-03-12 16:47:27 -0600

    style: enforce space before/after curly brackets

--- packages/bot/src/lib/extensions/TypicalMessage.ts
+++ packages/bot/src/lib/extensions/TypicalMessage.ts
@@ -65,7 +65,7 @@ export class TypicalMessage extends Structures.get('Message') {
     }
 
     respond(content: string, embed?: MessageEmbed) {
-        return this.channel.send(`${this.author} | ${content}`, {embed});
+        return this.channel.send(`${this.author} | ${content}`, { embed });
     }
 
     send(content: string | MessageEmbed,
