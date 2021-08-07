bedc315529cf1c14a42205ba7e34a951fafbb2c2
Nicholas Sylke <nicksylke@outlook.com>
2021-08-07 16:08:53 -0500

    refactor: remove wait before editing reply with latency in ping command

--- src/commands/PingCommand.ts
+++ src/commands/PingCommand.ts
@@ -1,12 +1,7 @@
 import Command from '../lib/command/Command';
-import { promisify } from 'util';
-
-const wait = promisify(setTimeout);
 
 const PingCommand: Command = async (client, interaction) => {
-    await interaction.reply('Pong!');
-    await wait(2000);
-    await interaction.editReply(`Pong! Latency: ${client.ws.ping}ms`);
+    await interaction.reply(`Pong! Latency: ${client.ws.ping}ms`);
 };
 
 PingCommand.options = {
