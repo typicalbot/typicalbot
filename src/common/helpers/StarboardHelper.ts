995119925507faf053419eecbf503f6630d011db
Nicholas Sylke <nicksylke@outlook.com>
2021-10-27 16:00:36 -0500

    chore: fix lint

--- src/common/helpers/StarboardHelper.ts
+++ src/common/helpers/StarboardHelper.ts
@@ -3,7 +3,7 @@ import type { GuildStarboard } from '../types/TypicalBotGuild';
 
 const createStarboard = async (database: Database, starboard: GuildStarboard): Promise<void> => {
     await database.insert('typicalbot_starboard', starboard);
-}
+};
 
 const getStarboard = async (database: Database, guildId: string): Promise<GuildStarboard | null> => {
     const starboard = await database.get('typicalbot_starboard', {
@@ -15,21 +15,21 @@ const getStarboard = async (database: Database, guildId: string): Promise<GuildS
     }
 
     return starboard;
-}
+};
 
 const updateStarboard = async (database: Database, guildId: string, starboard: GuildStarboard): Promise<void> => {
     await database.update('typicalbot_starboard', {
         guildId: guildId,
     }, starboard);
-}
+};
 
 const deleteStarboard = async (database: Database, guildId: string): Promise<void> => {
     await database.delete('typicalbot_starboard', { guildId: guildId });
-}
+};
 
 export {
     createStarboard,
     getStarboard,
     updateStarboard,
     deleteStarboard,
-}
+};
